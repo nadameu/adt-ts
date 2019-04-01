597f0d91f1b7cca97be472e6e7704c5ffdbcaccc
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-01 18:59:52 -0300

    New rules

--- src/Prelude/Control/Semigroupoid.ts
+++ src/Prelude/Control/Semigroupoid.ts
@@ -1,36 +1,25 @@
-import { SemigroupoidInstances } from '../../TypesDictionary';
+import { Desc, TypeDesc } from '../../Type';
 
-export interface Semigroupoid<a extends keys> {
-	compose: <acd extends SemigroupoidInstance<a>>(
-		_: acd,
-	) => <abc extends SemigroupoidMake<a, acd, any, SemigroupoidParams<a, acd>[0]>>(
-		_: abc,
-	) => SemigroupoidMake<a, acd, SemigroupoidParams<a, abc>[0], SemigroupoidParams<a, acd>[1]>;
+export interface Dict<params extends any[] = any[], descs extends TypeDesc[] = never[]> {
+	Fn: (_: params[0]) => params[1];
+}
+export type Type<a extends TypeDesc, params extends any[] = any[]> = a extends keyof Dict
+	? Dict<params>[a]
+	: a extends Desc<infer key, infer descs>
+	? key extends keyof Dict
+		? Dict<params, descs>[key]
+		: never
+	: never;
+
+export interface Semigroupoid<a extends TypeDesc> {
+	compose: <c, d>(_: Type<a, [c, d]>) => <b>(_: Type<a, [b, c]>) => Type<a, [b, d]>;
 }
 
-type keys = keyof SemigroupoidInstances;
-type run<
-	fn extends keyof SemigroupoidInstances[keys],
-	key extends keys,
-	args extends any[] = any[]
-> = SemigroupoidInstances<args>[key][fn];
-type SemigroupoidInstance<a extends keys> = run<'instance', a>;
-type SemigroupoidMake<a extends keys, abc extends SemigroupoidInstance<a>, d, e> = run<
-	'make',
-	a,
-	[abc, d, e]
->;
-type SemigroupoidParams<a extends keys, abc extends SemigroupoidInstance<a>> = run<
-	'params',
-	a,
-	[abc]
->;
+export const semigroupoidFn: Semigroupoid<'Fn'> = {
+	compose: f => g => x => f(g(x)),
+};
 
-export const composeFlipped = <a extends keys>(A: Semigroupoid<a>) => <
-	abc extends SemigroupoidInstance<a>
->(
-	abc: abc,
-) => <acd extends SemigroupoidMake<a, abc, SemigroupoidParams<a, abc>[1], any>>(
-	acd: acd,
-): SemigroupoidMake<a, abc, SemigroupoidParams<a, abc>[0], SemigroupoidParams<a, acd>[1]> =>
-	A.compose(acd)(abc);
+export const composeFlipped: <a extends TypeDesc>(
+	S: Semigroupoid<a>,
+) => <b, c>(_: Type<a, [b, c]>) => <d>(_: Type<a, [c, d]>) => Type<a, [b, d]> = S => f => g =>
+	S.compose(g)(f);
