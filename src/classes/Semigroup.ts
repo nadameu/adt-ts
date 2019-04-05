eabf956189f9383267ff2844fb6e9893794a3544
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-05 16:51:11 -0300

    Improvement

--- src/classes/Semigroup.ts
+++ src/classes/Semigroup.ts
@@ -1,11 +1,14 @@
 import * as fl from '../fantasy-land';
 import { HKT } from '../HKT';
 import { Keys, Type } from '../Types';
+import { equals } from './Setoid';
 
 export interface Semigroup<S extends Keys, a, b, c, d> extends HKT<S, a, b, c, d> {
-	[fl.concat]: (_: Type<S, a, b, c, d>) => Type<S, a, b, c, d>;
+	[fl.concat]: (this: Type<S, a, b, c, d>, _: TS<S, a, b, c, d>) => TS<S, a, b, c, d>;
 }
 
+type TS<S extends Keys, a, b, c, d> = Type<S, a, b, c, d> & Semigroup<S, a, b, c, d>;
+
 export const concat = <S extends Keys, a = never, b = never, c = never, d = never>(
-	x: Semigroup<S, a, b, c, d>,
-) => x[fl.concat];
+	x: TS<S, a, b, c, d>,
+) => (y: TS<S, a, b, c, d>) => x[fl.concat](y);
