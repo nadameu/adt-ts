805db89cb979d0d4e79ef0ae88e2f75209d0f8ad
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-05 16:51:24 -0300

    Bug correction

--- src/HKT.ts
+++ src/HKT.ts
@@ -1,8 +1,8 @@
 import { Keys } from './Types';
 export interface HKT<URI extends Keys, A, B, C, D> {
 	'@@URI': URI;
-	'@@A'?: A;
-	'@@B'?: B;
-	'@@C'?: C;
-	'@@D'?: D;
+	'@@A': A;
+	'@@B': B;
+	'@@C': C;
+	'@@D': D;
 }
