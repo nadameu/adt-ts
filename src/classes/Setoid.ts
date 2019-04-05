f50ab008cc7a890f306b07795c008a934be6742e
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-05 15:44:53 -0300

    Shorthand

--- src/classes/Setoid.ts
+++ src/classes/Setoid.ts
@@ -3,12 +3,11 @@ import { HKT } from '../HKT';
 import { Keys, Type } from '../Types';
 
 export interface Setoid<S extends Keys, a, b, c, d> extends HKT<S, a, b, c, d> {
-	[fl.equals]: (
-		this: Type<S, a, b, c, d>,
-		_: Type<S, a, b, c, d> & Setoid<S, a, b, c, d>,
-	) => boolean;
+	[fl.equals]: (this: Type<S, a, b, c, d>, _: TS<S, a, b, c, d>) => boolean;
 }
 
+type TS<S extends Keys, a, b, c, d> = Type<S, a, b, c, d> & Setoid<S, a, b, c, d>;
+
 export const equals = <S extends Keys, a = never, b = never, c = never, d = never>(
-	x: Type<S, a, b, c, d> & Setoid<S, a, b, c, d>,
-) => (y: Type<S, a, b, c, d> & Setoid<S, a, b, c, d>) => x[fl.equals](y);
+	x: TS<S, a, b, c, d>,
+) => (y: TS<S, a, b, c, d>) => x[fl.equals](y);
