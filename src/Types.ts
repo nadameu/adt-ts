97953f16816df65dd79f2c8ac5a96f95069a2511
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-08 14:23:33 -0300

    Obfuscation

--- src/Types.ts
+++ src/Types.ts
@@ -2,4 +2,4 @@ export interface Types<w, x, y, z> {
 	never: never;
 }
 export type Keys = keyof Types<never, never, never, never>;
-export type Type<key extends Keys, w, x, y, z> = Types<w, x, y, z>[key];
+export type Type<key extends Keys, w, x, y, z> = {} & Types<w, x, y, z>[key];
