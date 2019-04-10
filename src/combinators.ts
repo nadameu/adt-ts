3ff96c683769283e708b082a4942eba92f99c5bf
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-10 16:32:50 -0300

    Better types

--- src/combinators.ts
+++ src/combinators.ts
@@ -1,41 +1,64 @@
-export type fn<a, b> = (_: a) => b;
-export type fn2<a, b, c> = (_: a) => (_: b) => c;
-export type fn3<a, b, c, d> = (_: a) => (_: b) => (_: c) => d;
-
-export const A: <a, b>(f: fn<a, b>) => (x: a) => b = f => x => f(x);
+export const A: <a, b>(f: (_: a) => b) => (x: a) => b = f => x => f(x);
 export { A as apply };
 
-export const B: <b, c>(f: fn<b, c>) => <a>(g: fn<a, b>) => fn<a, c> = f => g => x => f(g(x));
+export const B: {
+	<a, b, c>(f: (_: b) => c): (g: (_: a) => b) => (_: a) => c;
+	<b, c>(f: (_: b) => c): <a>(g: (_: a) => b) => (_: a) => c;
+} = (f: any) => (g: any) => (x: any): any => f(g(x));
 export { B as compose };
 
+export const compose_: <a, b, c>(f: (_: b) => c, g: (_: a) => b) => (_: a) => c = (f, g) => x =>
+	f(g(x));
+
 export const B1: <c, d>(
-	f: fn<c, d>,
-) => <a, b>(g: fn2<a, b, c>) => fn2<a, b, d> = f => g => x => y => f(g(x)(y));
+	f: (_: c) => d,
+) => <a, b>(g: (_: a) => (_: b) => c) => (_: a) => (_: b) => d = f => g => x => y => f(g(x)(y));
 
 export const B2: <d, e>(
-	f: fn<d, e>,
-) => <a, b, c>(g: fn3<a, b, c, d>) => fn3<a, b, c, e> = f => g => x => y => z => f(g(x)(y)(z));
+	f: (_: d) => e,
+) => <a, b, c>(
+	g: (_: a) => (_: b) => (_: c) => d,
+) => (_: a) => (_: b) => (_: c) => e = f => g => x => y => z => f(g(x)(y)(z));
 
 export const B3: <c, d>(
-	f: fn<c, d>,
-) => <b>(g: fn<b, c>) => <a>(h: fn<a, b>) => fn<a, d> = f => g => h => x => f(g(h(x)));
+	f: (_: c) => d,
+) => <b>(g: (_: b) => c) => <a>(h: (_: a) => b) => (_: a) => d = f => g => h => x => f(g(h(x)));
 
-export const C: <a, b, c>(f: fn2<a, b, c>) => fn2<b, a, c> = f => y => x => f(x)(y);
+export const C: <a, b, c>(f: (_: a) => (_: b) => c) => (_: b) => (_: a) => c = f => y => x =>
+	f(x)(y);
 export { C as flip };
 
 export const I: <a>(x: a) => a = x => x;
 export { I as identity };
 
-export const K: <a>(x: a) => <b>(y: b) => a = x => _ => x;
+export const K: {
+	<a, b>(x: a): (y: b) => a;
+	<a>(x: a): <b>(y: b) => a;
+} = (x: any) => (_: any): any => x;
 export { K as constant };
 
-export const KI: <a>(x: a) => <b>(y: b) => b = K(I);
+export const KI: {
+	<a, b>(x: a): (y: b) => b;
+	<a>(x: a): <b>(y: b) => b;
+} = K(I);
 
-export const P: <b, c>(f: fn2<b, b, c>) => <a>(g: fn<a, b>) => fn2<a, a, c> = f => g => x => y =>
-	f(g(x))(g(y));
+export const P: <b, c>(
+	f: (_: b) => (_: b) => c,
+) => <a>(g: (_: a) => b) => (_: a) => (_: a) => c = f => g => x => y => f(g(x))(g(y));
 export { P as on };
 
-export const T: <a>(x: a) => <b>(f: fn<a, b>) => b = C<fn<any, any>, any, any>(A);
+export const pipe: {
+	<a, b, c>(f: (_: a) => b): (g: (_: b) => c) => (_: a) => c;
+	<a, b>(f: (_: a) => b): <c>(g: (_: b) => c) => (_: a) => c;
+} = (f: any) => (g: any) => (x: any) => g(f(x));
+export const pipe_: <a, b, c>(f: (_: a) => b, g: (_: b) => c) => (_: a) => c = (f, g) => x =>
+	g(f(x));
+
+export const T: {
+	<a, b>(x: a): (f: (_: a) => b) => b;
+	<a>(x: a): <b>(f: (_: a) => b) => b;
+} = C(A) as any;
 export { T as thrush };
 
-export const T2: <a>(x: a) => <b>(y: b) => <c>(f: fn2<a, b, c>) => c = x => y => f => f(x)(y);
+export const T2: <a>(x: a) => <b>(y: b) => <c>(f: (_: a) => (_: b) => c) => c = x => y => f =>
+	f(x)(y);
