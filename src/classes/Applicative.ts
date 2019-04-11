ce3bc5c02cde5e1a5c9118bc056257cab1be1644
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-11 17:50:08 -0300

    Instance -> Static

--- src/classes/Applicative.ts
+++ src/classes/Applicative.ts
@@ -1,40 +1,14 @@
-import { fn } from '../combinators';
-import * as fl from '../fantasy-land';
-import { Keys, Type } from '../Types';
-import { ap, Apply } from './Apply';
+import { Type1 } from '../Types';
+import { Apply } from './Apply';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Applicative<fa extends Applicative<any, a>, a> extends Apply<fa, a> {}
-export interface ApplicativeConstructor<f extends Applicative<any, any>> {
-	prototype: f;
-	[fl.of]<a, y = never, x = never, w = never>(_: a): CreateType<f, a, y, x, w>;
+export interface Applicative<f> extends Apply<f> {
+	pure: <a, y = never, x = never, w = never>(value: a) => Type1<f, w, x, y, a>;
 }
 
-type CreateType<f extends Applicative<any, any>, a, y, x, w> = {} & {
-	[key in Keys]: f extends Type<key, any, any, any, any> ? Type<key, w, x, y, a> : never
-}[Keys];
-type Create<f extends Applicative<any, any>, a, y, x, w> = {} & Extract<
-	CreateType<f, a, y, x, w>,
-	Applicative<any, a>
->;
-type DeriveType<f extends Applicative<any, any>, a> = {} & {
-	[key in Keys]: f extends Type<key, infer w, infer x, infer y, any> ? Type<key, w, x, y, a> : never
-}[Keys];
-type Derive<f extends Applicative<any, any>, a> = {} & Extract<
-	DeriveType<f, a>,
-	Applicative<any, a>
->;
+export const pure: <f>(A: Applicative<f>) => Applicative<f>['pure'] = A => A.pure;
 
-export const of = <f extends Applicative<any, any>>(A: ApplicativeConstructor<f>) => <
-	a,
-	y = never,
-	x = never,
-	w = never
->(
-	value: a,
-) => A[fl.of]<a, y, x, w>(value) as Create<f, a, y, x, w>;
-
-export const liftA1: <a, b>(
-	f: fn<a, b>,
-) => <fa extends Applicative<any, a>>(fa: fa) => Derive<fa, b> = f => fa =>
-	ap(of(fa.constructor as any)(f) as any)(fa as any) as any;
+export const liftA1: <f>(
+	A: Applicative<f>,
+) => <a, b>(
+	f: (_: a) => b,
+) => <y, x, w>(fa: Type1<f, w, x, y, a>) => Type1<f, w, x, y, b> = A => f => A.ap(A.pure(f));
