e47818df1f2d6bba0931be7d81e6f6daa06e71ee
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-11 17:20:18 -0300

    Refactoring

--- src/classes/Functor.ts
+++ src/classes/Functor.ts
@@ -1,4 +1,4 @@
-import { constant, flip } from '../combinators';
+import { compose, constant, flip, thrush } from '../combinators';
 import { Type1 } from '../Types';
 
 export interface Functor<f> {
@@ -9,22 +9,26 @@ export const map: <f>(F: Functor<f>) => Functor<f>['map'] = F => F.map;
 
 export const mapFlipped: <f>(
 	F: Functor<f>,
-) => <a, y, x, w>(
-	fa: Type1<f, w, x, y, a>,
-) => <b>(f: (_: a) => b) => Type1<f, w, x, y, b> = F => fa => f => map(F)(f)(fa);
+) => <a, y, x, w>(fa: Type1<f, w, x, y, a>) => <b>(f: (_: a) => b) => Type1<f, w, x, y, b> = F =>
+	flip<any, any, any>(F.map);
 
 export const voidLeft: <f>(
 	F: Functor<f>,
 ) => <y, x, w>(fa: Type1<f, w, x, y, any>) => <b>(y: b) => Type1<f, w, x, y, b> = F =>
-	flip<any, any, any>(y => map(F)(constant(y)));
+	flip<any, any, any>(voidRight(F));
 
 export const voidRight: <f>(
 	F: Functor<f>,
-) => <a>(x: a) => <y, x, w>(fb: Type1<f, w, x, y, any>) => Type1<f, w, x, y, a> = F => x =>
-	map(F)(constant(x));
+) => <a>(x: a) => <y, x, w>(fb: Type1<f, w, x, y, any>) => Type1<f, w, x, y, a> = F =>
+	compose<any, any, any>(F.map)(constant);
 
 const _void: <f>(
 	F: Functor<f>,
 ) => <y, x, w>(fa: Type1<f, w, x, y, any>) => Type1<f, w, x, y, void> = F =>
-	map(F)(constant(undefined));
+	F.map(constant(undefined));
 export { _void as void };
+
+export const flap: <f>(
+	F: Functor<f>,
+) => <a, b, y, x, w>(ff: Type1<f, w, x, y, (_: a) => b>) => (x: a) => Type1<f, w, x, y, b> = F =>
+	compose<any, any, any>(F.map)(thrush);
