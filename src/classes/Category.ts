253d28df661ab62f04743ad1b09454e2bc9580c7
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-12 16:06:49 -0300

    Instance -> static

--- src/classes/Category.ts
+++ src/classes/Category.ts
@@ -1,21 +1,8 @@
-import * as fl from '../fantasy-land';
-import { Keys, Type } from '../Types';
+import { Type2 } from '../Types';
 import { Semigroupoid } from './Semigroupoid';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Category<c extends Category<any, a, b>, a, b> extends Semigroupoid<c, a, b> {}
-export interface CategoryConstructor<c extends Category<any, any, any>> {
-	prototype: c;
-	[fl.id]: CreateType<c>;
+export interface Category<f> extends Semigroupoid<f> {
+	identity: <x = never, w = never>() => Type2<f, w, x, any, any>;
 }
 
-type CreateType<c extends Category<any, any, any>> = {} & {
-	[key in Keys]: c extends Type<key, any, any, any, any> ? Type<key, any, any, any, any> : never
-}[Keys];
-type Create<c extends Category<any, any, any>> = {} & Extract<
-	CreateType<c>,
-	Category<any, any, any>
->;
-
-export const id = <c extends Category<any, any, any>>(C: CategoryConstructor<c>) =>
-	C[fl.id] as Create<c>;
+export const identity: <f>(C: Category<f>) => Category<f>['identity'] = C => C.identity;
