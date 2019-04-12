af6ef93dce68d1295254f3e72c9628c5e921f1db
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-12 14:33:20 -0300

    Instance -> static

--- src/classes/Setoid.ts
+++ src/classes/Setoid.ts
@@ -1,8 +1,7 @@
-import * as fl from '../fantasy-land';
-
-export interface Setoid<a extends Setoid<a>> {
-	[fl.equals]: (this: a, _: a) => boolean;
+export interface Setoid<a> {
+	equals: (x: a) => (y: a) => boolean;
 }
 
-export const equals = <a extends Setoid<a>>(x: a) => (y: a) => x[fl.equals](y);
-export const notEquals = <a extends Setoid<a>>(x: a) => (y: a) => !x[fl.equals](y);
+export const equals: <a>(S: Setoid<a>) => Setoid<a>['equals'] = S => S.equals;
+
+export const notEquals: <a>(S: Setoid<a>) => Setoid<a>['equals'] = S => x => y => !S.equals(x)(y);
