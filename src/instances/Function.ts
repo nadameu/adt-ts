2752deb61b2110a4a51c2d9f7241e7bc68f9f31b
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-12 14:23:14 -0300

    Function

--- src/instances/Function.ts
+++ src/instances/Function.ts
@@ -1,26 +1,12 @@
-import * as fl from '../fantasy-land';
+import { Semigroupoid } from '../classes/Semigroupoid';
+import { compose } from '../combinators';
+import { Placeholder as _ } from '../Types';
 
-export interface Fn<a, b> {
-	(_: a): b;
-	[fl.compose]<c>(that: Fn<b, c>): Fn<a, c>;
-}
-export interface FnConstructor {
-	<a, b>(f: (_: a) => b): Fn<a, b>;
-	prototype: Fn<any, any>;
-	[fl.id]: Fn<any, any>;
-}
+export type Fn<a, b> = (_: a) => b;
 
-const cloneFn: <args extends any[], b>(f: (...args: args) => b) => (...args: args) => b = f =>
-	function(this: any) {
-		return f.apply(this, arguments as any);
-	};
+export { compose };
 
-export const Fn: FnConstructor = (<a, b>(f: (_: a) => b): Fn<a, b> =>
-	Object.assign(cloneFn(f), Fn.prototype)) as any;
-Fn[fl.id] = Fn(<a>(x: a) => x);
-Fn.prototype[fl.compose] = function(that) {
-	return Fn(x => that(this(x)));
-};
+export const semigroupoidFunction: Semigroupoid<Fn<_, _>> = { compose };
 
 declare module '../Types' {
 	export interface Types<w, x, y, z> {
