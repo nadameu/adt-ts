08a6db859013a52e0d4d10c86f65a7bf028a2792
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-12 16:59:52 -0300

    PureScript name

--- src/classes/Apply.ts
+++ src/classes/Apply.ts
@@ -3,19 +3,19 @@ import { Type1 } from '../Types';
 import { Functor, map } from './Functor';
 
 export interface Apply<f> extends Functor<f> {
-	ap: <a, b, y, x, w>(
+	apply: <a, b, y, x, w>(
 		ff: Type1<f, w, x, y, (_: a) => b>,
 	) => (fa: Type1<f, w, x, y, a>) => Type1<f, w, x, y, b>;
 }
 
-export const ap: <f>(A: Apply<f>) => Apply<f>['ap'] = A => A.ap;
+export const apply: <f>(A: Apply<f>) => Apply<f>['apply'] = A => A.apply;
 
-export const apFlipped: <f>(
+export const applyFlipped: <f>(
 	A: Apply<f>,
 ) => <a, y, x, w>(
 	fa: Type1<f, w, x, y, a>,
 ) => <b>(ff: Type1<f, w, x, y, (_: a) => b>) => Type1<f, w, x, y, b> = A =>
-	flip<any, any, any>(A.ap);
+	flip<any, any, any>(A.apply);
 
 export const applyFirst: <f>(
 	A: Apply<f>,
@@ -30,15 +30,14 @@ export const applySecond: <f>(
 ) => <a>(fb: Type1<f, w, x, y, a>) => Type1<f, w, x, y, a> = A =>
 	lift2(A)<any, any, any>(constant(identity));
 
-export const lift1: <f>(A: Apply<f>) => Functor<f>['map'] = map;
-
 export const lift2: <f>(
 	A: Apply<f>,
 ) => <a, b, c>(
 	f: (_: a) => (_: b) => c,
 ) => <y, x, w>(
 	fa: Type1<f, w, x, y, a>,
-) => (fb: Type1<f, w, x, y, b>) => Type1<f, w, x, y, c> = A => B1<any, any>(A.ap)<any, any>(A.map);
+) => (fb: Type1<f, w, x, y, b>) => Type1<f, w, x, y, c> = A =>
+	B1<any, any>(A.apply)<any, any>(A.map);
 
 export const lift3: <f>(
 	A: Apply<f>,
@@ -47,4 +46,4 @@ export const lift3: <f>(
 ) => <y, x, w>(
 	fa: Type1<f, w, x, y, a>,
 ) => (fb: Type1<f, w, x, y, b>) => (fc: Type1<f, w, x, y, c>) => Type1<f, w, x, y, d> = A =>
-	B2<any, any>(A.ap)<any, any, any>(lift2(A));
+	B2<any, any>(A.apply)<any, any, any>(lift2(A));
