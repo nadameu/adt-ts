73af78e8dd15762851779a73ce7309460ea3aee8
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-12 17:18:30 -0300

    Refactoring

--- src/classes/Applicative.ts
+++ src/classes/Applicative.ts
@@ -1,6 +1,5 @@
 import { Type1 } from '../Types';
 import { Apply } from './Apply';
-import { Functor } from './Functor';
 
 export interface Applicative<f> extends Apply<f> {
 	pure: <a, y = never, x = never, w = never>(value: a) => Type1<f, w, x, y, a>;
@@ -8,8 +7,9 @@ export interface Applicative<f> extends Apply<f> {
 
 export const pure: <f>(A: Applicative<f>) => Applicative<f>['pure'] = A => A.pure;
 
-export const liftA1: <f>(A: Pick<Applicative<f>, 'apply' | 'pure'>) => Functor<f>['map'] = A => f =>
-	A.apply(A.pure(f));
+export const liftA1: <f>(
+	A: Pick<Applicative<f>, 'apply' | 'pure'>,
+) => Applicative<f>['map'] = A => f => A.apply(A.pure(f));
 
 export const when: <f>(
 	A: Applicative<f>,
