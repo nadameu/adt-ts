d7e8aa80a50b2c7320a595730eb94476b2af05c0
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-12 14:44:21 -0300

    Instance -> static

--- src/classes/Ord.ts
+++ src/classes/Ord.ts
@@ -1,30 +1,36 @@
-import * as fl from '../fantasy-land';
 import { Ordering } from '../instances/Ordering';
 import { Setoid } from './Setoid';
 
-export interface Ord<a extends Ord<a>> extends Setoid<a> {
-	[fl.lte]: (this: a, _: a) => boolean;
+export interface Ord<a> extends Setoid<a> {
+	compare: (x: a) => (y: a) => Ordering;
 }
 
-export const lte = <a extends Ord<a>>(x: a) => (y: a) => x[fl.lte](y);
+export const compare: <a>(O: Ord<a>) => Ord<a>['compare'] = O => O.compare;
 
-export const gt = <a extends Ord<a>>(x: a) => (y: a): boolean => !x[fl.lte](y);
+export const lte: <a>(O: Ord<a>) => (x: a) => (y: a) => boolean = O => x => y =>
+	O.compare(x)(y) !== Ordering.GT;
 
-export const lt = <a extends Ord<a>>(x: a) => (y: a): boolean => !y[fl.lte](x);
+export const gt: <a>(O: Ord<a>) => (x: a) => (y: a) => boolean = O => x => y =>
+	O.compare(x)(y) === Ordering.GT;
 
-export const gte = <a extends Ord<a>>(x: a) => (y: a): boolean => y[fl.lte](x);
+export const lt: <a>(O: Ord<a>) => (x: a) => (y: a) => boolean = O => x => y =>
+	O.compare(x)(y) === Ordering.LT;
 
-export const compare = <a extends Ord<a>>(x: a) => (y: a): Ordering =>
-	!y[fl.lte](x) ? Ordering.LT : !x[fl.lte](y) ? Ordering.GT : Ordering.EQ;
+export const gte: <a>(O: Ord<a>) => (x: a) => (y: a) => boolean = O => x => y =>
+	O.compare(x)(y) !== Ordering.LT;
 
-export const comparing = <a, b extends Ord<b>>(f: (_: a) => b) => (x: a) => (y: a) =>
-	compare(f(x))(f(y));
+export const comparing: <b>(
+	O: Ord<b>,
+) => <a>(f: (_: a) => b) => (x: a) => (y: a) => Ordering = O => f => x => y =>
+	O.compare(f(x))(f(y));
 
-export const min = <a extends Ord<a>>(x: a) => (y: a): a => (lte(x)(y) ? x : y);
+export const min: <a>(O: Ord<a>) => (x: a) => (y: a) => a = O => x => y => (lte(O)(x)(y) ? x : y);
 
-export const max = <a extends Ord<a>>(x: a) => (y: a): a => (gte(x)(y) ? x : y);
+export const max: <a>(O: Ord<a>) => (x: a) => (y: a) => a = O => x => y => (gte(O)(x)(y) ? x : y);
 
-export const clamp = <a extends Ord<a>>(lo: a) => (hi: a) => (x: a): a => min(hi)(max(lo)(x));
+export const clamp: <a>(O: Ord<a>) => (lo: a) => (hi: a) => (x: a) => a = O => lo => hi => x =>
+	min(O)(hi)(max(O)(lo)(x));
 
-export const between = <a extends Ord<a>>(lo: a) => (hi: a) => (x: a): boolean =>
-	gte(x)(lo) && lte(x)(hi);
+export const between: <a>(
+	O: Ord<a>,
+) => (lo: a) => (hi: a) => (x: a) => boolean = O => lo => hi => x => gte(O)(x)(lo) && lte(O)(x)(hi);
