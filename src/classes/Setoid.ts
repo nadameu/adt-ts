d85c11d26a31f8dfc3f379681994eb3c9e0e583b
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-12 16:34:37 -0300

    PureScript names

--- src/classes/Setoid.ts
+++
@@ -1,7 +0,0 @@
-export interface Setoid<a> {
-	equals: (x: a) => (y: a) => boolean;
-}
-
-export const equals: <a>(S: Setoid<a>) => Setoid<a>['equals'] = S => S.equals;
-
-export const notEquals: <a>(S: Setoid<a>) => Setoid<a>['equals'] = S => x => y => !S.equals(x)(y);
