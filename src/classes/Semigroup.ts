e4cef7ffca51323409ca4d209325079e2f87a41c
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-12 15:59:22 -0300

    PureScript name

--- src/classes/Semigroup.ts
+++ src/classes/Semigroup.ts
@@ -1,5 +1,5 @@
 export interface Semigroup<a> {
-	concat: (x: a) => (y: a) => a;
+	append: (x: a) => (y: a) => a;
 }
 
-export const concat: <a>(S: Semigroup<a>) => Semigroup<a>['concat'] = S => S.concat;
+export const append: <a>(S: Semigroup<a>) => Semigroup<a>['append'] = S => S.append;
