5c362eabe2910d94c35bc87d5cf9f43e9d37b6a1
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-25 17:12:23 -0300

    New type system

--- src/classes/Bind.ts
+++ src/classes/Bind.ts
@@ -1,45 +1,47 @@
-import { flip, identity } from '../combinators';
-import { Type1 } from '../Types';
+import { identity } from '../combinators';
+import { Prop1, Type } from '../Types';
 import { Apply } from './Apply';
 
-export interface Bind<f> extends Apply<f> {
-	bind: <a, y, x, w>(
-		fa: Type1<f, w, x, y, a>,
-	) => <b>(f: (_: a) => Type1<f, w, x, y, b>) => Type1<f, w, x, y, b>;
+export interface Bind<f extends Prop1> extends Apply<f> {
+	bind: <a, y, x, w, v>(
+		fa: Type<f, v, w, x, y, a>,
+	) => <b>(f: (_: a) => Type<f, v, w, x, y, b>) => Type<f, v, w, x, y, b>;
 }
 
-export const bind: <f>(C: Bind<f>) => Bind<f>['bind'] = B => B.bind;
+export const bindFlipped: <f extends Prop1>(
+	B: Pick<Bind<f>, 'bind'>,
+) => <a, b, y, x, w, v>(
+	f: (_: a) => Type<f, v, w, x, y, b>,
+) => (fa: Type<f, v, w, x, y, a>) => Type<f, v, w, x, y, b> = ({ bind }) => f => fa => bind(fa)(f);
 
-export const bindFlipped: <f>(
-	B: Bind<f>,
-) => <a, b, y, x, w>(
-	f: (_: a) => Type1<f, w, x, y, b>,
-) => (fa: Type1<f, w, x, y, a>) => Type1<f, w, x, y, b> = B => flip<any, any, any>(B.bind);
+export const join: <f extends Prop1>(
+	B: Pick<Bind<f>, 'bind'>,
+) => <a, y, x, w, v>(fa: Type<f, v, w, x, y, Type<f, v, w, x, y, a>>) => Type<f, v, w, x, y, a> = ({
+	bind,
+}) => bindFlipped({ bind })(identity);
 
-export const join: <f>(
-	B: Bind<f>,
-) => <a, y, x, w>(fa: Type1<f, w, x, y, Type1<f, w, x, y, a>>) => Type1<f, w, x, y, a> = B =>
-	bindFlipped(B)(identity);
+export const composeKleisli: <f extends Prop1>(
+	B: Pick<Bind<f>, 'bind'>,
+) => <a, b, y, x, w, v>(
+	f: (_: a) => Type<f, v, w, x, y, b>,
+) => <c>(g: (_: b) => Type<f, v, w, x, y, c>) => (_: a) => Type<f, v, w, x, y, c> = ({
+	bind,
+}) => f => g => x => bind(f(x))(g);
 
-export const composeKleisli: <f>(
-	B: Bind<f>,
-) => <a, b, y, x, w>(
-	f: (_: a) => Type1<f, w, x, y, b>,
-) => <c>(g: (_: b) => Type1<f, w, x, y, c>) => (_: a) => Type1<f, w, x, y, c> = B => f => g => x =>
-	B.bind(f(x))(g);
+export const composeKleisliFlipped: <f extends Prop1>(
+	B: Pick<Bind<f>, 'bind'>,
+) => <b, c, y, x, w, v>(
+	f: (_: b) => Type<f, v, w, x, y, c>,
+) => <a>(g: (_: a) => Type<f, v, w, x, y, b>) => (_: a) => Type<f, v, w, x, y, c> = ({
+	bind,
+}) => f => g => x => bind(g(x))(f);
 
-export const composeKleisliFlipped: <f>(
-	B: Bind<f>,
-) => <b, c, y, x, w>(
-	f: (_: b) => Type1<f, w, x, y, c>,
-) => <a>(g: (_: a) => Type1<f, w, x, y, b>) => (_: a) => Type1<f, w, x, y, c> = B => f => g => x =>
-	B.bind(g(x))(f);
-
-export const ifM: <f>(
-	B: Bind<f>,
-) => <y, x, w>(
-	cond: Type1<f, w, x, y, boolean>,
+export const ifM: <f extends Prop1>(
+	B: Pick<Bind<f>, 'bind'>,
+) => <y, x, w, v>(
+	cond: Type<f, v, w, x, y, boolean>,
 ) => <a>(
-	t: Type1<f, w, x, y, a>,
-) => (f: Type1<f, w, x, y, a>) => Type1<f, w, x, y, a> = B => cond => t => f =>
-	B.bind(cond)(x => (x ? t : f));
+	whenTrue: Type<f, v, w, x, y, a>,
+) => (whenFalse: Type<f, v, w, x, y, a>) => Type<f, v, w, x, y, a> = ({
+	bind,
+}) => cond => whenTrue => whenFalse => bind(cond)(x => (x ? whenTrue : whenFalse));
