ab0fd6678ab5a765e47226ea14e6c313af15ed13
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-25 16:58:18 -0300

    new type system

--- src/classes/Functor.ts
+++ src/classes/Functor.ts
@@ -1,34 +1,36 @@
-import { compose, constant, flip, thrush } from '../combinators';
-import { Type1 } from '../Types';
+import { constant, thrush } from '../combinators';
+import { Prop1, Type } from '../Types';
 
-export interface Functor<f> {
-	map: <a, b>(f: (_: a) => b) => <y, x, w>(fa: Type1<f, w, x, y, a>) => Type1<f, w, x, y, b>;
+export interface Functor<f extends Prop1> {
+	map: <a, b>(f: (_: a) => b) => <y, x, w, v>(fa: Type<f, v, w, x, y, a>) => Type<f, v, w, x, y, b>;
 }
 
-export const map: <f>(F: Functor<f>) => Functor<f>['map'] = F => F.map;
-
-export const mapFlipped: <f>(
+export const mapFlipped: <f extends Prop1>(
 	F: Functor<f>,
-) => <a, y, x, w>(fa: Type1<f, w, x, y, a>) => <b>(f: (_: a) => b) => Type1<f, w, x, y, b> = F =>
-	flip<any, any, any>(F.map);
+) => <a, y, x, w, v>(
+	fa: Type<f, v, w, x, y, a>,
+) => <b>(f: (_: a) => b) => Type<f, v, w, x, y, b> = ({ map }) => fa => f => map(f)(fa);
 
-export const voidLeft: <f>(
+export const voidLeft: <f extends Prop1>(
 	F: Functor<f>,
-) => <y, x, w>(fa: Type1<f, w, x, y, any>) => <b>(y: b) => Type1<f, w, x, y, b> = F =>
-	flip<any, any, any>(voidRight(F));
+) => <y, x, w, v>(fa: Type<f, v, w, x, y, any>) => <b>(y: b) => Type<f, v, w, x, y, b> = ({
+	map,
+}) => fa => y => map(constant(y))(fa);
 
-export const voidRight: <f>(
+export const voidRight: <f extends Prop1>(
 	F: Functor<f>,
-) => <a>(x: a) => <y, x, w>(fb: Type1<f, w, x, y, any>) => Type1<f, w, x, y, a> = F =>
-	compose<any, any, any>(F.map)(constant);
+) => <a>(x: a) => <y, x, w, v>(fb: Type<f, v, w, x, y, any>) => Type<f, v, w, x, y, a> = ({
+	map,
+}) => x => map(constant(x));
 
-const _void: <f>(
+const _void: <f extends Prop1>(
 	F: Functor<f>,
-) => <y, x, w>(fa: Type1<f, w, x, y, any>) => Type1<f, w, x, y, void> = F =>
-	F.map(constant(undefined));
+) => <y, x, w, v>(fa: Type<f, v, w, x, y, any>) => Type<f, v, w, x, y, void> = ({ map }) =>
+	map(constant(undefined));
 export { _void as void };
 
-export const flap: <f>(
+export const flap: <f extends Prop1>(
 	F: Functor<f>,
-) => <a, b, y, x, w>(ff: Type1<f, w, x, y, (_: a) => b>) => (x: a) => Type1<f, w, x, y, b> = F =>
-	compose<any, any, any>(F.map)(thrush);
+) => <a, b, y, x, w, v>(
+	ff: Type<f, v, w, x, y, (_: a) => b>,
+) => (x: a) => Type<f, v, w, x, y, b> = ({ map }) => ff => x => mapFlipped({ map })(ff)(thrush(x));
