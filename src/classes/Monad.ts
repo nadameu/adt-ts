ae68e5e048a588d41a2a78a4f503e7cfb6698b60
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-25 18:17:59 -0300

    new type system

--- src/classes/Monad.ts
+++ src/classes/Monad.ts
@@ -1,26 +1,30 @@
 import { compose, flip } from '../combinators';
-import { Type1 } from '../Types';
+import { Prop1, Type } from '../Types';
 import { Applicative, unless, when } from './Applicative';
-import { Bind } from './Bind';
+import { Bind, bindFlipped } from './Bind';
 
-export interface Monad<f> extends Applicative<f>, Bind<f> {}
+export interface Monad<f extends Prop1> extends Applicative<f>, Bind<f> {}
 
-export const liftM1: <f>(M: Pick<Monad<f>, 'bind' | 'pure'>) => Monad<f>['map'] = M =>
-	compose<any, any>(flip<any, any, any>(M.bind))(compose(M.pure));
+export const liftM1: <f extends Prop1>(M: Pick<Monad<f>, 'bind' | 'pure'>) => Monad<f>['map'] = ({
+	bind,
+	pure,
+}) => f => bindFlipped({ bind })(x => pure(f(x)));
 
-export const ap: <f>(M: Pick<Monad<f>, 'bind' | 'pure'>) => Monad<f>['apply'] = M => ff => fa =>
-	M.bind(ff)(f => M.bind(fa)(x => M.pure(f(x))));
+export const ap: <f extends Prop1>(M: Pick<Monad<f>, 'bind' | 'pure'>) => Monad<f>['apply'] = ({
+	bind,
+	pure,
+}) => ff => fa => bind(ff)(f => bind(fa)(x => pure(f(x))));
 
-export const whenM: <f>(
+export const whenM: <f extends Prop1>(
 	A: Pick<Monad<f>, 'bind' | 'pure'>,
-) => <y, x, w>(
-	fa: Type1<f, w, x, y, boolean>,
-) => (fa: Type1<f, w, x, y, void>) => Type1<f, w, x, y, void> = M => fp => fa =>
-	M.bind(fp)(flip<any, any, any>(when(M))(fa));
+) => <y, x, w, v>(
+	fp: Type<f, v, w, x, y, boolean>,
+) => (fa: Type<f, v, w, x, y, void>) => Type<f, v, w, x, y, void> = ({ bind, pure }) => fp => fa =>
+	bind(fp)(p => when({ pure })(p)(fa));
 
-export const unlessM: <f>(
+export const unlessM: <f extends Prop1>(
 	A: Pick<Monad<f>, 'bind' | 'pure'>,
-) => <y, x, w>(
-	fa: Type1<f, w, x, y, boolean>,
-) => (fa: Type1<f, w, x, y, void>) => Type1<f, w, x, y, void> = M => fp => fa =>
-	M.bind(fp)(p => unless(M)(p)(fa));
+) => <y, x, w, v>(
+	fp: Type<f, v, w, x, y, boolean>,
+) => (fa: Type<f, v, w, x, y, void>) => Type<f, v, w, x, y, void> = ({ bind, pure }) => fp => fa =>
+	bind(fp)(p => unless({ pure })(p)(fa));
