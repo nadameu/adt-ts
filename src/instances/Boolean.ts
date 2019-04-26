c08e60cadefd841d4e3c5f39df9441a75bce34c8
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-26 18:39:22 -0300

    Boolean

--- src/instances/Boolean.ts
+++ src/instances/Boolean.ts
@@ -1,19 +1,20 @@
-import * as fl from '../fantasy-land';
+import * as E from '../classes/Eq';
+import { Eq } from '../classes/Eq';
+import * as O from '../classes/Ord';
+import { Ord } from '../classes/Ord';
+import { Ordering } from './Ordering';
 
-Boolean.prototype[fl.equals] = function equals(that) {
-	return this === that;
-};
+export const eq: Eq<boolean>['eq'] = x => y => x === y;
+export const notEq = E.notEq({ eq });
 
-export default {};
-
-declare global {
-	interface Boolean {
-		['fantasy-land/equals']: (this: boolean, _: boolean) => boolean;
-	}
-}
-
-declare module '../Types' {
-	export interface Types<w, x, y, z> {
-		Boolean: boolean;
-	}
-}
+export const compare: Ord<boolean>['compare'] = x => y =>
+	x === y ? Ordering.EQ : x < y ? Ordering.LT : Ordering.GT;
+export const lte = O.lte<boolean>({ compare });
+export const gt = O.gt<boolean>({ compare });
+export const lt = O.lt<boolean>({ compare });
+export const gte = O.gte<boolean>({ compare });
+export const comparing = O.comparing<boolean>({ compare });
+export const min = O.min<boolean>({ compare });
+export const max = O.max<boolean>({ compare });
+export const clamp = O.clamp<boolean>({ compare });
+export const between = O.between<boolean>({ compare });
