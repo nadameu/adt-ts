41e1279f7a585599d6f73015e14270c6e2597e6c
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-26 18:52:23 -0300

    Revert "Unused"
    This reverts commit 3b860ba87f1d0fed41045124dc0bd66f00232cc1.

--- src/classes/Semigroup.ts
+++ src/classes/Semigroup.ts
@@ -1,8 +1,11 @@
-import { Prop1, Type1 } from '../Types';
+import { Prop1, Prop2, Type1, Type2 } from '../Types';
 
 export interface Semigroup<a> {
 	append: (x: a) => (y: a) => a;
 }
 export interface Semigroup1<f extends Prop1> {
 	append: <a>(fx: Type1<f, a>) => (fy: Type1<f, a>) => Type1<f, a>;
+}
+export interface Semigroup2<f extends Prop2> {
+	append: <a, b>(fx: Type2<f, a, b>) => (fy: Type2<f, a, b>) => Type2<f, a, b>;
 }
