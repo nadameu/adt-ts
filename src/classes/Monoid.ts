3b860ba87f1d0fed41045124dc0bd66f00232cc1
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-26 18:24:14 -0300

    Unused

--- src/classes/Monoid.ts
+++ src/classes/Monoid.ts
@@ -1,5 +1,5 @@
-import { Prop1, Prop2, Type1, Type2 } from '../Types';
-import { Semigroup, Semigroup1, Semigroup2 } from './Semigroup';
+import { AnyFn2, AnyFn3, Prop1, Type1 } from '../Types';
+import { Semigroup, Semigroup1 } from './Semigroup';
 
 export interface Monoid<a> extends Semigroup<a> {
 	mempty: () => a;
@@ -7,6 +7,4 @@ export interface Monoid<a> extends Semigroup<a> {
 export interface Monoid1<f extends Prop1> extends Semigroup1<f> {
 	mempty: <a = never>() => Type1<f, a>;
 }
-export interface Monoid2<f extends Prop2> extends Semigroup2<f> {
-	mempty: <a = never, b = never>() => Type2<f, a, b>;
-}
+
