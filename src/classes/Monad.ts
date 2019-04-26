682613e37f7bde3c67fda09e0bac7683a75b7bb5
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-26 15:39:37 -0300

    Prettier types

--- src/classes/Monad.ts
+++ src/classes/Monad.ts
@@ -1,30 +1,42 @@
-import { compose, flip } from '../combinators';
-import { Prop1, Type } from '../Types';
-import { Applicative, unless, when } from './Applicative';
-import { Bind, bindFlipped } from './Bind';
+import { Prop1, Prop2, Type1, Type2 } from '../Types';
+import { Applicative1, Applicative2, unless, when } from './Applicative';
+import { Bind1, Bind2, bindFlipped } from './Bind';
 
-export interface Monad<f extends Prop1> extends Applicative<f>, Bind<f> {}
+export interface Monad1<f extends Prop1> extends Applicative1<f>, Bind1<f> {}
+export interface Monad2<f extends Prop2> extends Applicative2<f>, Bind2<f> {}
 
-export const liftM1: <f extends Prop1>(M: Pick<Monad<f>, 'bind' | 'pure'>) => Monad<f>['map'] = ({
-	bind,
-	pure,
-}) => f => bindFlipped({ bind })(x => pure(f(x)));
+export const liftM1: {
+	<f extends Prop2>(M: Pick<Monad2<f>, 'bind' | 'pure'>): Monad2<f>['map'];
+	<f extends Prop1>(M: Pick<Monad1<f>, 'bind' | 'pure'>): Monad1<f>['map'];
+} = (({ bind, pure }) => f => bindFlipped({ bind })(x => pure(f(x)))) as (<f extends Prop1>(
+	M: Pick<Monad1<f>, 'bind' | 'pure'>,
+) => Monad1<f>['map']);
 
-export const ap: <f extends Prop1>(M: Pick<Monad<f>, 'bind' | 'pure'>) => Monad<f>['apply'] = ({
-	bind,
-	pure,
-}) => ff => fa => bind(ff)(f => bind(fa)(x => pure(f(x))));
+export const ap: {
+	<f extends Prop2>(M: Pick<Monad2<f>, 'bind' | 'pure'>): Monad2<f>['apply'];
+	<f extends Prop1>(M: Pick<Monad1<f>, 'bind' | 'pure'>): Monad1<f>['apply'];
+} = (({ bind, pure }) => ff => fa => bind(ff)(f => bind(fa)(x => pure(f(x))))) as <f extends Prop1>(
+	M: Pick<Monad1<f>, 'bind' | 'pure'>,
+) => Monad1<f>['apply'];
 
-export const whenM: <f extends Prop1>(
-	A: Pick<Monad<f>, 'bind' | 'pure'>,
-) => <y, x, w, v>(
-	fp: Type<f, v, w, x, y, boolean>,
-) => (fa: Type<f, v, w, x, y, void>) => Type<f, v, w, x, y, void> = ({ bind, pure }) => fp => fa =>
-	bind(fp)(p => when({ pure })(p)(fa));
+export const whenM: {
+	<f extends Prop2>(A: Pick<Monad2<f>, 'bind' | 'pure'>): <a>(
+		fp: Type2<f, a, boolean>,
+	) => (fa: Type2<f, a, void>) => Type2<f, a, void>;
+	<f extends Prop1>(A: Pick<Monad1<f>, 'bind' | 'pure'>): (
+		fp: Type1<f, boolean>,
+	) => (fa: Type1<f, void>) => Type1<f, void>;
+} = (({ bind, pure }) => fp => fa => bind(fp)(p => when({ pure })(p)(fa))) as <f extends Prop1>(
+	A: Pick<Monad1<f>, 'bind' | 'pure'>,
+) => (fp: Type1<f, boolean>) => (fa: Type1<f, void>) => Type1<f, void>;
 
-export const unlessM: <f extends Prop1>(
-	A: Pick<Monad<f>, 'bind' | 'pure'>,
-) => <y, x, w, v>(
-	fp: Type<f, v, w, x, y, boolean>,
-) => (fa: Type<f, v, w, x, y, void>) => Type<f, v, w, x, y, void> = ({ bind, pure }) => fp => fa =>
-	bind(fp)(p => unless({ pure })(p)(fa));
+export const unlessM: {
+	<f extends Prop2>(A: Pick<Monad2<f>, 'bind' | 'pure'>): <a>(
+		fp: Type2<f, a, boolean>,
+	) => (fa: Type2<f, a, void>) => Type2<f, a, void>;
+	<f extends Prop1>(A: Pick<Monad1<f>, 'bind' | 'pure'>): (
+		fp: Type1<f, boolean>,
+	) => (fa: Type1<f, void>) => Type1<f, void>;
+} = (({ bind, pure }) => fp => fa => bind(fp)(p => unless({ pure })(p)(fa))) as <f extends Prop1>(
+	A: Pick<Monad1<f>, 'bind' | 'pure'>,
+) => (fp: Type1<f, boolean>) => (fa: Type1<f, void>) => Type1<f, void>;
