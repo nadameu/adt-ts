a9b80a7877999c834380ecc92e4c51a57591549c
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-04-26 17:08:12 -0300

    Prettier types

--- src/classes/Monoid.ts
+++ src/classes/Monoid.ts
@@ -1,6 +1,12 @@
-import { Prop0, Type } from '../Types';
-import { Semigroup } from './Semigroup';
+import { Prop1, Prop2, Type1, Type2 } from '../Types';
+import { Semigroup, Semigroup1, Semigroup2 } from './Semigroup';
 
-export interface Monoid<a extends Prop0> extends Semigroup<a> {
-	empty: <z = never, y = never, x = never, w = never, v = never>() => Type<a, v, w, x, y, z>;
+export interface Monoid<a> extends Semigroup<a> {
+	mempty: () => a;
+}
+export interface Monoid1<f extends Prop1> extends Semigroup1<f> {
+	mempty: <a = never>() => Type1<f, a>;
+}
+export interface Monoid2<f extends Prop2> extends Semigroup2<f> {
+	mempty: <a = never, b = never>() => Type2<f, a, b>;
 }
