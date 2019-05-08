4c103c26dd0a0d4da5664a152524f6b7035d9aeb
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-05-08 14:49:58 -0300

    Cleanup

--- src/classes/Bifunctor.ts
+++ src/classes/Bifunctor.ts
@@ -5,12 +5,15 @@ export interface Bifunctor2<f extends Prop2> {
 	bimap: <a, c>(f: (_: a) => c) => <b, d>(g: (_: b) => d) => (fx: Type2<f, a, b>) => Type2<f, c, d>;
 }
 
-export const lmap: <f extends Prop2>(
-	B: Bifunctor2<f>,
-) => <a, c>(f: (_: a) => c) => <b>(fx: Type2<f, a, b>) => Type2<f, c, b> = ({ bimap }) => f =>
-	bimap(f)(identity);
+interface Derived2<f extends Prop2> extends Bifunctor2<f> {
+	lmap: <a, c>(f: (_: a) => c) => <b>(fx: Type2<f, a, b>) => Type2<f, c, b>;
+	rmap: <b, c>(f: (_: b) => c) => <a>(fx: Type2<f, a, b>) => Type2<f, a, c>;
+}
 
-export const rmap: <f extends Prop2>(
-	B: Bifunctor2<f>,
-) => <b, c>(g: (_: b) => c) => <a>(fx: Type2<f, a, b>) => Type2<f, a, c> = ({ bimap }) =>
-	bimap(identity);
+type Derive<k extends keyof Bifunctor2<never>, r extends keyof Derived2<never>> = <f extends Prop2>(
+	B: Pick<Bifunctor2<f>, k>,
+) => Derived2<f>[r];
+
+export const lmap: Derive<'bimap', 'lmap'> = ({ bimap }) => f => bimap(f)(identity);
+
+export const rmap: Derive<'bimap', 'rmap'> = ({ bimap }) => bimap(identity);
