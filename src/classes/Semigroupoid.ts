819f929304171e91703d83e242b0db6cb652b434
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-05-08 18:13:48 -0300

    Cleanup

--- src/classes/Semigroupoid.ts
+++ src/classes/Semigroupoid.ts
@@ -1,11 +1,16 @@
+import { flip } from '../instances/Fn';
 import { Prop2, Type2 } from '../Types';
 
 export interface Semigroupoid2<f extends Prop2> {
 	compose: <b, c>(f: Type2<f, b, c>) => <a>(g: Type2<f, a, b>) => Type2<f, a, c>;
 }
 
-export const composeFlipped: {
-	<f extends Prop2>(S: Semigroupoid2<f>): <a, b>(
-		f: Type2<f, a, b>,
-	) => <c>(g: Type2<f, b, c>) => Type2<f, a, c>;
-} = ({ compose }) => f => g => compose(g)(f);
+interface Derived2<f extends Prop2> extends Semigroupoid2<f> {
+	composeFlipped: <a, b>(f: Type2<f, a, b>) => <c>(g: Type2<f, b, c>) => Type2<f, a, c>;
+}
+
+type Derive<r extends keyof Derived2<never>> = <f extends Prop2>(
+	S: Semigroupoid2<f>,
+) => Derived2<f>[r];
+
+export const composeFlipped: Derive<'composeFlipped'> = ({ compose }) => flip(compose);
