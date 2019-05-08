ca8c8e3d3c8181ead83388c39fe1200f9a0a64c6
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-05-08 16:45:05 -0300

    Cleanup

--- src/classes/Foldable.ts
+++ src/classes/Foldable.ts
@@ -1,107 +1,132 @@
-import { compose, composeFlipped, flip, identity } from '../instances/Fn';
-import { Prop1, Prop2, Type1, Type2, AnyFn2 } from '../Types';
-import { Monoid, Monoid1, Monoid2 } from './Monoid';
-import { Monad2, Monad1 } from './Monad';
+import { compose, composeFlipped, flip, identity, thrush } from '../instances/Fn';
+import { Prop1, Prop2, Type1, Type2 } from '../Types';
+import { Monad1, Monad2 } from './Monad';
+import { Monoid, Monoid1 } from './Monoid';
 
 export interface Foldable1<f extends Prop1> {
 	foldr: <a, b>(f: (_: a) => (_: b) => b) => (seed: b) => (fa: Type1<f, a>) => b;
 	foldl: <a, b>(f: (_: b) => (_: a) => b) => (seed: b) => (fa: Type1<f, a>) => b;
-	foldMap: {
-		<m extends Prop2>(M: Monoid2<m>): <a, b, c>(
-			f: (_: a) => Type2<m, b, c>,
-		) => (fa: Type1<f, a>) => Type2<m, b, c>;
-		<m extends Prop1>(M: Monoid1<m>): <a, b>(
-			f: (_: a) => Type1<m, b>,
-		) => (fa: Type1<f, a>) => Type1<m, b>;
-		<m>(M: Monoid<m>): <a>(f: (_: a) => m) => (fa: Type1<f, a>) => m;
-	};
+	foldMap: Orig1<f, 'foldMap'>;
 }
 
 export interface Foldable2<f extends Prop2> {
 	foldr: <b, c>(f: (_: b) => (_: c) => c) => (seed: c) => <a>(fa: Type2<f, a, b>) => c;
 	foldl: <b, c>(f: (_: c) => (_: b) => c) => (seed: c) => <a>(fa: Type2<f, a, b>) => c;
-	foldMap: {
-		<m extends Prop2>(M: Monoid2<m>): <b, c, d>(
-			f: (_: b) => Type2<m, c, d>,
-		) => <a>(fa: Type2<f, a, b>) => Type2<m, c, d>;
-		<m extends Prop1>(M: Monoid1<m>): <b, c>(
-			f: (_: b) => Type1<m, c>,
-		) => <a>(fa: Type2<f, a, b>) => Type1<m, c>;
-		<m>(M: Monoid<m>): <b>(f: (_: b) => m) => <a>(fa: Type2<f, a, b>) => m;
+	foldMap: Orig2<f, 'foldMap'>;
+}
+
+interface Derived10<f extends Prop1, m> extends Pick<Foldable1<f>, 'foldr' | 'foldl'> {
+	foldMap: <a>(f: (_: a) => m) => (fa: Type1<f, a>) => m;
+	fold: (fa: Type1<f, m>) => m;
+}
+interface Derived11<f extends Prop1, m extends Prop1>
+	extends Pick<Foldable1<f>, 'foldr' | 'foldl'> {
+	foldMap: <a, b>(f: (_: a) => Type1<m, b>) => (fa: Type1<f, a>) => Type1<m, b>;
+	fold: <a>(fa: Type1<f, Type1<m, a>>) => Type1<m, a>;
+	foldM: <a, b>(f: (_: a) => (_: b) => Type1<m, a>) => (a0: a) => (xs: Type1<f, b>) => Type1<m, a>;
+}
+interface Derived12<f extends Prop1, m extends Prop2>
+	extends Pick<Foldable1<f>, 'foldr' | 'foldl'> {
+	foldM: <a, b, c>(
+		f: (_: b) => (_: c) => Type2<m, a, b>,
+	) => (a0: b) => (xs: Type1<f, c>) => Type2<m, a, b>;
+}
+interface Derived20<f extends Prop2, m> extends Pick<Foldable2<f>, 'foldr' | 'foldl'> {
+	foldMap: <b>(f: (_: b) => m) => <a>(fa: Type2<f, a, b>) => m;
+	fold: <a>(fa: Type2<f, a, m>) => m;
+}
+interface Derived21<f extends Prop2, m extends Prop1>
+	extends Pick<Foldable2<f>, 'foldr' | 'foldl'> {
+	foldMap: <b, c>(f: (_: b) => Type1<m, c>) => <a>(fa: Type2<f, a, b>) => Type1<m, c>;
+	fold: <a, b>(fa: Type2<f, a, Type1<m, b>>) => Type1<m, b>;
+	foldM: <b, c>(
+		f: (_: b) => (_: c) => Type1<m, b>,
+	) => (a0: b) => <a>(xs: Type2<f, a, c>) => Type1<m, b>;
+}
+interface Derived22<f extends Prop2, m extends Prop2>
+	extends Pick<Foldable1<f>, 'foldr' | 'foldl'> {
+	foldM: <a, c, d>(
+		f: (_: c) => (_: d) => Type2<m, a, c>,
+	) => (a0: c) => <b>(xs: Type2<f, b, d>) => Type2<m, a, c>;
+}
+
+interface Orig1<f extends Prop1, r extends keyof Derived10<never, never>> {
+	<m extends Prop1>(M: Monoid1<m>): Derived11<f, m>[r];
+	<m>(M: Monoid<m>): Derived10<f, m>[r];
+}
+interface Orig2<f extends Prop2, r extends keyof Derived20<never, never>> {
+	<m extends Prop1>(M: Monoid1<m>): Derived21<f, m>[r];
+	<m>(M: Monoid<m>): Derived20<f, m>[r];
+}
+
+type DeriveM<k extends keyof Foldable1<never>, r extends keyof Derived10<never, never>> = <
+	f extends Prop1
+>(
+	F: Pick<Foldable1<f>, k>,
+) => <m>(M: Monoid<m>) => Derived10<f, m>[r];
+interface DeriveAllM<k extends keyof Foldable1<never>, r extends keyof Derived10<never, never>> {
+	<f extends Prop2>(F: Pick<Foldable2<f>, k>): {
+		<m extends Prop1>(M: Monoid1<m>): Derived21<f, m>[r];
+		<m>(M: Monoid<m>): Derived20<f, m>[r];
 	};
+	<f extends Prop1>(F: Pick<Foldable1<f>, k>): {
+		<m extends Prop1>(M: Monoid1<m>): Derived11<f, m>[r];
+		<m>(M: Monoid<m>): Derived10<f, m>[r];
+	};
+}
+
+type DeriveMd<k extends keyof Foldable1<never>, r extends keyof Derived12<never, never>> = <
+	f extends Prop1
+>(
+	F: Pick<Foldable1<f>, k>,
+) => <m extends Prop1>(M: Monad1<m>) => Derived11<f, m>[r];
+interface DeriveAllMd<k extends keyof Foldable1<never>, r extends keyof Derived12<never, never>> {
+	<f extends Prop2>(F: Pick<Foldable2<f>, k>): {
+		<m extends Prop2>(M: Monad2<m>): Derived22<f, m>[r];
+		<m extends Prop1>(M: Monad1<m>): Derived21<f, m>[r];
+	};
+	<f extends Prop1>(F: Pick<Foldable1<f>, k>): {
+		<m extends Prop2>(M: Monad2<m>): Derived12<f, m>[r];
+		<m extends Prop1>(M: Monad1<m>): Derived11<f, m>[r];
+	};
+}
+
+type Derive<k extends keyof Foldable1<never>, r extends keyof Derived10<never, never>> = <
+	f extends Prop1
+>(
+	F: Pick<Foldable1<f>, k>,
+) => Derived10<f, never>[r];
+interface DeriveAll<k extends keyof Foldable1<never>, r extends keyof Derived10<never, never>> {
+	<f extends Prop2>(F: Pick<Foldable2<f>, k>): Derived20<f, never>[r];
+	<f extends Prop1>(F: Pick<Foldable1<f>, k>): Derived10<f, never>[r];
 }
 
 interface PropEndo extends Prop1 {
 	type: (_: this['a']) => this['a'];
 }
-export const foldrDefault: {
-	<f extends Prop2>(F: Pick<Foldable2<f>, 'foldMap'>): Foldable2<f>['foldr'];
-	<f extends Prop1>(F: Pick<Foldable1<f>, 'foldMap'>): Foldable1<f>['foldr'];
-} = <f extends Prop1>({ foldMap }: Pick<Foldable1<f>, 'foldMap'>) => <a, b>(
-	f: (_: a) => (_: b) => b,
-) => (seed: b) => (xs: Type1<f, a>) =>
-	foldMap<PropEndo>({ append: compose, mempty: () => identity })(f)(xs)(seed);
+export const foldrDefault: DeriveAll<'foldMap', 'foldr'> = (({ foldMap }) => f => seed => xs =>
+	foldMap<PropEndo>({ append: compose, mempty: () => identity })(f)(xs)(seed)) as Derive<
+	'foldMap',
+	'foldr'
+>;
 
-export const foldlDefault: {
-	<f extends Prop2>(F: Pick<Foldable2<f>, 'foldMap'>): Foldable2<f>['foldl'];
-	<f extends Prop1>(F: Pick<Foldable1<f>, 'foldMap'>): Foldable1<f>['foldl'];
-} = <f extends Prop1>({ foldMap }: Pick<Foldable1<f>, 'foldMap'>) => <a, b>(
-	f: (_: b) => (_: a) => b,
-) => (seed: b) => (xs: Type1<f, a>) =>
-	foldMap<PropEndo>({ append: composeFlipped, mempty: () => identity })(flip(f))(xs)(seed);
+export const foldlDefault: DeriveAll<'foldMap', 'foldl'> = (({ foldMap }) => f => seed => xs =>
+	foldMap<PropEndo>({ append: composeFlipped, mempty: () => identity })(flip(f))(xs)(
+		seed,
+	)) as Derive<'foldMap', 'foldl'>;
 
-export const foldMapDefaultR: {
-	<f extends Prop2>(F: Pick<Foldable2<f>, 'foldr'>): Foldable2<f>['foldMap'];
-	<f extends Prop1>(F: Pick<Foldable1<f>, 'foldr'>): Foldable1<f>['foldMap'];
-} = <f extends Prop1>({ foldr }: Pick<Foldable1<f>, 'foldr'>) => <m>({
+export const foldMapDefaultR: DeriveAllM<'foldr', 'foldMap'> = (({ foldr }) => ({
 	append,
 	mempty,
-}: Monoid<m>) => <a>(f: (_: a) => m) => (fa: Type1<f, a>): m =>
-	foldr<a, m>(x => acc => append(f(x))(acc))(mempty())(fa);
+}) => f => thrush(mempty())(foldr(x => append(f(x))))) as DeriveM<'foldr', 'foldMap'>;
 
-export const foldMapDefaultL: {
-	<f extends Prop2>(F: Pick<Foldable2<f>, 'foldl'>): Foldable2<f>['foldMap'];
-	<f extends Prop1>(F: Pick<Foldable1<f>, 'foldl'>): Foldable1<f>['foldMap'];
-} = <f extends Prop1>({ foldl }: Pick<Foldable1<f>, 'foldl'>) => <m>({
+export const foldMapDefaultL: DeriveAllM<'foldl', 'foldMap'> = (({ foldl }) => ({
 	append,
 	mempty,
-}: Monoid<m>) => <a>(f: (_: a) => m) => (fa: Type1<f, a>): m =>
-	foldl<a, m>(acc => x => append(acc)(f(x)))(mempty())(fa);
+}) => f => thrush(mempty())(foldl(acc => x => append(acc)(f(x))))) as DeriveM<'foldl', 'foldMap'>;
 
-export const fold: {
-	<f extends Prop2>(F: Pick<Foldable2<f>, 'foldMap'>): {
-		<m extends Prop2>(M: Monoid2<m>): <a, b, c>(fa: Type2<f, a, Type2<m, b, c>>) => Type2<m, b, c>;
-		<m extends Prop1>(M: Monoid1<m>): <a, b>(fa: Type2<f, a, Type1<m, b>>) => Type1<m, b>;
-		<b>(M: Monoid<b>): <a>(fa: Type2<f, a, b>) => b;
-	};
-	<f extends Prop1>(F: Pick<Foldable1<f>, 'foldMap'>): {
-		<m extends Prop2>(M: Monoid2<m>): <a, b>(fa: Type1<f, Type2<m, a, b>>) => Type2<m, a, b>;
-		<m extends Prop1>(M: Monoid1<m>): <a>(fa: Type1<f, Type1<m, a>>) => Type1<m, a>;
-		<a>(M: Monoid<a>): (fa: Type1<f, a>) => a;
-	};
-} = (({ foldMap }) => M => foldMap(M)(identity)) as AnyFn2;
+export const fold: DeriveAllM<'foldMap', 'fold'> = (({ foldMap }) =>
+	flip(foldMap)(identity)) as DeriveM<'foldMap', 'fold'>;
 
-export const foldM: {
-	<f extends Prop2>(F: Pick<Foldable2<f>, 'foldl'>): {
-		<m extends Prop2>(M: Pick<Monad2<m>, 'bind' | 'pure'>): <a, b, d>(
-			f: (_: b) => (_: d) => Type2<m, a, b>,
-		) => (a0: b) => <c>(xs: Type2<f, c, d>) => Type2<m, a, b>;
-		<m extends Prop1>(M: Pick<Monad1<m>, 'bind' | 'pure'>): <a, c>(
-			f: (_: a) => (_: c) => Type1<m, a>,
-		) => (a0: a) => <b>(xs: Type2<f, b, c>) => Type1<m, a>;
-	};
-	<f extends Prop1>(F: Pick<Foldable1<f>, 'foldl'>): {
-		<m extends Prop2>(M: Pick<Monad2<m>, 'bind' | 'pure'>): <a, b, c>(
-			f: (_: b) => (_: c) => Type2<m, a, b>,
-		) => (a0: b) => (xs: Type1<f, c>) => Type2<m, a, b>;
-		<m extends Prop1>(M: Pick<Monad1<m>, 'bind' | 'pure'>): <a, b>(
-			f: (_: a) => (_: b) => Type1<m, a>,
-		) => (a0: a) => (xs: Type1<f, b>) => Type1<m, a>;
-	};
-} = <f extends Prop1>({ foldl }: Pick<Foldable1<f>, 'foldl'>) => <m extends Prop1>({
-	bind,
-	pure,
-}: Pick<Monad1<m>, 'bind' | 'pure'>) => <a, b>(f: (_: a) => (_: b) => Type1<m, a>) => (
-	a0: a,
-): ((xs: Type1<f, b>) => Type1<m, a>) =>
-	foldl<b, Type1<m, a>>(ma => b => bind(ma)(flip(f)(b)))(pure(a0));
+export const foldM: DeriveAllMd<'foldl', 'foldM'> = (({ foldl }) => ({ bind, pure }) => f => seed =>
+	thrush(pure(seed))(foldl(ma => y => bind(ma)(flip(f)(y))))) as DeriveMd<'foldl', 'foldM'>;
