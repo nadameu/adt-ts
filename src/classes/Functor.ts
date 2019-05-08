7893c6516d7dbc3c9e1be58c042c520adefdcfc6
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-05-08 16:55:33 -0300

    Cleanup

--- src/classes/Functor.ts
+++ src/classes/Functor.ts
@@ -1,5 +1,5 @@
-import { constant, thrush } from '../instances/Fn';
-import { AnyFn1, AnyFn2, AnyFn3, Prop1, Prop2, Type1, Type2 } from '../Types';
+import { constant, flip, thrush } from '../instances/Fn';
+import { Prop1, Prop2, Type1, Type2 } from '../Types';
 
 export interface Functor1<f extends Prop1> {
 	map: <a, b>(f: (_: a) => b) => (fa: Type1<f, a>) => Type1<f, b>;
@@ -8,32 +8,38 @@ export interface Functor2<f extends Prop2> {
 	map: <b, c>(f: (_: b) => c) => <a>(fa: Type2<f, a, b>) => Type2<f, a, c>;
 }
 
-export const mapFlipped: {
-	<f extends Prop2>(F: Functor2<f>): <a, b>(
-		fa: Type2<f, a, b>,
-	) => <c>(f: (_: b) => c) => Type2<f, a, c>;
-	<f extends Prop1>(F: Functor1<f>): <a>(fa: Type1<f, a>) => <b>(f: (_: a) => b) => Type1<f, b>;
-} = (({ map }) => fa => f => map(f)(fa)) as AnyFn3;
+interface Derived1<f extends Prop1> extends Functor1<f> {
+	mapFlipped: <a>(fa: Type1<f, a>) => <b>(f: (_: a) => b) => Type1<f, b>;
+	voidLeft: <a>(fa: Type1<f, a>) => <b>(y: b) => Type1<f, b>;
+	voidRight: <b>(x: b) => <a>(fb: Type1<f, a>) => Type1<f, b>;
+	void: <a>(fa: Type1<f, a>) => Type1<f, void>;
+	flap: <a, b>(ff: Type1<f, (_: a) => b>) => (x: a) => Type1<f, b>;
+}
+interface Derived2<f extends Prop2> extends Functor2<f> {
+	mapFlipped: <a, b>(fa: Type2<f, a, b>) => <c>(f: (_: b) => c) => Type2<f, a, c>;
+	voidLeft: <a, b>(fa: Type2<f, a, b>) => <c>(y: c) => Type2<f, a, c>;
+	voidRight: <c>(x: c) => <a, b>(fb: Type2<f, a, b>) => Type2<f, a, c>;
+	void: <a, b>(fa: Type2<f, a, b>) => Type2<f, a, void>;
+	flap: <a, b, c>(ff: Type2<f, a, (_: b) => c>) => (x: b) => Type2<f, a, c>;
+}
 
-export const voidLeft: {
-	<f extends Prop2>(F: Functor2<f>): <a, b>(fa: Type2<f, a, b>) => <c>(y: c) => Type2<f, a, c>;
-	<f extends Prop1>(F: Functor1<f>): <a>(fa: Type1<f, a>) => <b>(y: b) => Type1<f, b>;
-} = (({ map }) => fa => y => map(constant(y))(fa)) as AnyFn3;
+type Derive<r extends keyof Derived1<never>> = <f extends Prop1>(F: Functor1<f>) => Derived1<f>[r];
+interface DeriveAll<r extends keyof Derived1<never>> {
+	<f extends Prop2>(F: Functor2<f>): Derived2<f>[r];
+	<f extends Prop1>(F: Functor1<f>): Derived1<f>[r];
+}
 
-export const voidRight: {
-	<f extends Prop2>(F: Functor2<f>): <c>(x: c) => <a, b>(fb: Type2<f, a, b>) => Type2<f, a, c>;
-	<f extends Prop1>(F: Functor1<f>): <b>(x: b) => <a>(fb: Type1<f, a>) => Type1<f, b>;
-} = (({ map }) => x => map(constant(x))) as AnyFn2;
+export const mapFlipped: DeriveAll<'mapFlipped'> = (({ map }) => flip(map)) as Derive<'mapFlipped'>;
 
-const _void: {
-	<f extends Prop2>(F: Functor2<f>): <a, b>(fa: Type2<f, a, b>) => Type2<f, a, void>;
-	<f extends Prop1>(F: Functor1<f>): <a>(fa: Type1<f, a>) => Type1<f, void>;
-} = (({ map }) => map(constant(undefined))) as AnyFn1;
+export const voidLeft: DeriveAll<'voidLeft'> = (F => flip(voidRight(F))) as Derive<'voidLeft'>;
+
+export const voidRight: DeriveAll<'voidRight'> = (({ map }) => x => map(constant(x))) as Derive<
+	'voidRight'
+>;
+
+const _void: DeriveAll<'void'> = (({ map }) => map(constant(undefined))) as Derive<'void'>;
 export { _void as void };
 
-export const flap: {
-	<f extends Prop2>(F: Functor2<f>): <a, b, c>(
-		ff: Type2<f, a, (_: b) => c>,
-	) => (x: b) => Type2<f, a, c>;
-	<f extends Prop1>(F: Functor1<f>): <a, b>(ff: Type1<f, (_: a) => b>) => (x: a) => Type1<f, b>;
-} = (({ map }) => ff => x => map(thrush(x))(ff)) as AnyFn3;
+export const flap: DeriveAll<'flap'> = (({ map }) => ff => x => map(thrush(x))(ff)) as Derive<
+	'flap'
+>;
