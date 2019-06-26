7bfafc34ef64e8ebbee8d21515e541832c70164a
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-06-26 19:16:23 -0300

    Organization

--- src/instances/Fn.ts
+++ src/instances/Fn.ts
@@ -7,14 +7,15 @@ import { Bind2 } from '../classes/Bind';
 import { Extend1, Extend2 } from '../classes/Extend';
 import * as F from '../classes/Functor';
 import { Functor2 } from '../classes/Functor';
+import { Lazy2 } from '../classes/Lazy';
 import * as M from '../classes/Monad';
+import { Monad2 } from '../classes/Monad';
 import * as M1 from '../classes/Monoid';
 import { Monoid, Monoid1, Monoid2 } from '../classes/Monoid';
 import { Semigroup, Semigroup1, Semigroup2 } from '../classes/Semigroup';
 import * as S from '../classes/Semigroupoid';
 import { Semigroupoid2 } from '../classes/Semigroupoid';
 import { AnyFn1, AnyFn4, Prop1, Prop2, Type1 } from '../Types';
-import { Lazy, Lazy2 } from '../classes/Lazy';
 
 export interface PropFn extends Prop2 {
 	type: (_: this['a']) => this['b'];
@@ -32,39 +33,43 @@ export const composeFlipped = S.composeFlipped<PropFn>({ compose });
 export const identity: <a>(_: a) => a = x => x;
 
 export const map: Functor2<PropFn>['map'] = compose;
-export const mapFlipped = F.mapFlipped<PropFn>({ map });
-export const voidLeft = F.voidLeft<PropFn>({ map });
-export const voidRight = F.voidRight<PropFn>({ map });
-const _void = F.void<PropFn>({ map });
-export { _void as void };
-export const flap = F.flap<PropFn>({ map });
+export const functorFn: Functor2<PropFn> = { map };
+export const mapFlipped = F.mapFlipped(functorFn);
+export const voidLeft = F.voidLeft(functorFn);
+export const voidRight = F.voidRight(functorFn);
+export const _void = F.void(functorFn);
+export const flap = F.flap(functorFn);
 
 export const apply: Apply2<PropFn>['apply'] = ff => fa => x => ff(x)(fa(x));
-export const applyFlipped = Ap.applyFlipped<PropFn>({ apply });
-export const applyFirst = Ap.applyFirst<PropFn>({ apply, map });
-export const applySecond = Ap.applySecond<PropFn>({ apply, map });
-export const lift2 = Ap.lift2<PropFn>({ apply, map });
-export const lift3 = Ap.lift3<PropFn>({ apply, map });
-export const lift4 = Ap.lift4<PropFn>({ apply, map });
-export const lift5 = Ap.lift5<PropFn>({ apply, map });
+export const applyFn: Apply2<PropFn> = { apply, map };
+export const applyFlipped = Ap.applyFlipped(applyFn);
+export const applyFirst = Ap.applyFirst(applyFn);
+export const applySecond = Ap.applySecond(applyFn);
+export const lift2 = Ap.lift2(applyFn);
+export const lift3 = Ap.lift3(applyFn);
+export const lift4 = Ap.lift4(applyFn);
+export const lift5 = Ap.lift5(applyFn);
 
 export const bind: Bind2<PropFn>['bind'] = fa => f => x => f(fa(x))(x);
-export const bindFlipped = B.bindFlipped<PropFn>({ bind });
-export const join = B.join<PropFn>({ bind });
-export const composeKleisli = B.composeKleisli<PropFn>({ bind });
-export const composeKleisliFlipped = B.composeKleisliFlipped<PropFn>({ bind });
-export const ifM = B.ifM<PropFn>({ bind });
+export const bindFn: Bind2<PropFn> = { apply, bind, map };
+export const bindFlipped = B.bindFlipped(bindFn);
+export const join = B.join(bindFn);
+export const composeKleisli = B.composeKleisli(bindFn);
+export const composeKleisliFlipped = B.composeKleisliFlipped(bindFn);
+export const ifM = B.ifM(bindFn);
 
 export const pure: Applicative2<PropFn>['pure'] = x => _ => x;
-export const liftA1 = A.liftA1<PropFn>({ apply, pure });
-export const when = A.when<PropFn>({ pure });
-export const unless = A.unless<PropFn>({ pure });
+export const applicativeFn: Applicative2<PropFn> = { apply, map, pure };
+export const liftA1 = A.liftA1(applicativeFn);
+export const when = A.when(applicativeFn);
+export const unless = A.unless(applicativeFn);
 export const constant: <a>(x: a) => <b>(y: b) => a = pure;
 
-export const liftM1 = M.liftM1<PropFn>({ bind, pure });
-export const ap = M.ap<PropFn>({ bind, pure });
-export const whenM = M.whenM<PropFn>({ bind, pure });
-export const unlessM = M.unlessM<PropFn>({ bind, pure });
+export const monadFn: Monad2<PropFn> = { apply, bind, map, pure };
+export const liftM1 = M.liftM1(monadFn);
+export const ap = M.ap(monadFn);
+export const whenM = M.whenM(monadFn);
+export const unlessM = M.unlessM(monadFn);
 
 interface PropDerivedFn<f extends Prop1> extends Prop2 {
 	type: (_: this['a']) => Type1<f, this['b']>;
@@ -75,8 +80,8 @@ export const append1: {
 } = (({ append }) => lift2(append)) as AnyFn1;
 
 export const mempty1: {
-	<f extends Prop1>(S: Pick<Monoid1<f>, 'mempty'>): Monoid2<PropDerivedFn<f>>['mempty'];
-	<b>(S: Pick<Monoid<b>, 'mempty'>): Monoid1<PropFn1<b>>['mempty'];
+	<f extends Prop1>(S: Monoid1<f>): Monoid2<PropDerivedFn<f>>['mempty'];
+	<b>(S: Monoid<b>): Monoid1<PropFn1<b>>['mempty'];
 } = (({ mempty }) => () => () => mempty()) as AnyFn1;
 export const power1: {
 	<f extends Prop1>(M: Monoid1<f>): <a, b>(
@@ -85,11 +90,11 @@ export const power1: {
 	<b>(M: Monoid<b>): <a>(x: (_: a) => b) => (p: number) => (_: a) => b;
 } = (M => M1.power({ append: append1(M), mempty: mempty1(M) })) as AnyFn1;
 export const guard1: {
-	<f extends Prop1>(M: Pick<Monoid1<f>, 'mempty'>): (
+	<f extends Prop1>(M: Monoid1<f>): (
 		p: boolean,
 	) => <a, b>(x: (_: a) => Type1<f, b>) => (_: a) => Type1<f, b>;
-	<b>(M: Pick<Monoid<b>, 'mempty'>): (p: boolean) => <a>(x: (_: a) => b) => (_: a) => b;
-} = (({ mempty }) => M1.guard({ mempty: mempty1({ mempty }) })) as AnyFn1;
+	<b>(M: Monoid<b>): (p: boolean) => <a>(x: (_: a) => b) => (_: a) => b;
+} = (M => M1.guard({ mempty: mempty1(M) })) as AnyFn1;
 
 export const thrush: <a>(x: a) => <b>(f: (_: a) => b) => b = x => f => f(x);
 
