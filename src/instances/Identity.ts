0b82fe1044d3676e221760a4a194ccfbe4df5af0
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-07-01 19:02:50 -0300

    Organization

--- src/instances/Identity.ts
+++ src/instances/Identity.ts
@@ -10,6 +10,7 @@ import { Extend1 } from '../classes/Extend';
 import * as F from '../classes/Functor';
 import { Functor1 } from '../classes/Functor';
 import * as M from '../classes/Monad';
+import { Monad1 } from '../classes/Monad';
 import { Prop1 } from '../Types';
 
 declare const phantom: unique symbol;
@@ -23,43 +24,50 @@ interface PropIdentity extends Prop1 {
 }
 
 export const map: Functor1<PropIdentity>['map'] = f => x => Identity(f(x));
-export const mapFlipped = F.mapFlipped<PropIdentity>({ map });
-export const voidLeft = F.voidLeft<PropIdentity>({ map });
-export const voidRight = F.voidRight<PropIdentity>({ map });
-const _void = F.void<PropIdentity>({ map });
+export const functorIdentity: Functor1<PropIdentity> = { map };
+export const mapFlipped = F.mapFlipped(functorIdentity);
+export const voidLeft = F.voidLeft(functorIdentity);
+export const voidRight = F.voidRight(functorIdentity);
+const _void = F.void(functorIdentity);
 export { _void as void };
-export const flap = F.flap<PropIdentity>({ map });
+export const flap = F.flap(functorIdentity);
 
 export const apply: Apply1<PropIdentity>['apply'] = f => x => Identity(f(x));
-export const applyFlipped = Ap.applyFlipped<PropIdentity>({ apply });
-export const applyFirst = Ap.applyFirst<PropIdentity>({ apply, map });
-export const applySecond = Ap.applySecond<PropIdentity>({ apply, map });
-export const lift2 = Ap.lift2<PropIdentity>({ apply, map });
-export const lift3 = Ap.lift3<PropIdentity>({ apply, map });
-export const lift4 = Ap.lift4<PropIdentity>({ apply, map });
-export const lift5 = Ap.lift5<PropIdentity>({ apply, map });
+export const applyIdentity: Apply1<PropIdentity> = { apply, map };
+export const applyFlipped = Ap.applyFlipped(applyIdentity);
+export const applyFirst = Ap.applyFirst(applyIdentity);
+export const applySecond = Ap.applySecond(applyIdentity);
+export const lift2 = Ap.lift2(applyIdentity);
+export const lift3 = Ap.lift3(applyIdentity);
+export const lift4 = Ap.lift4(applyIdentity);
+export const lift5 = Ap.lift5(applyIdentity);
 
 export const bind: Bind1<PropIdentity>['bind'] = x => f => f(x);
-export const bindFlipped = B.bindFlipped<PropIdentity>({ bind });
-export const join = B.join<PropIdentity>({ bind });
-export const composeKleisli = B.composeKleisli<PropIdentity>({ bind });
-export const composeKleisliFlipped = B.composeKleisliFlipped<PropIdentity>({ bind });
-export const ifM = B.ifM<PropIdentity>({ bind });
+export const bindIdentity: Bind1<PropIdentity> = { apply, bind, map };
+export const bindFlipped = B.bindFlipped(bindIdentity);
+export const join = B.join(bindIdentity);
+export const composeKleisli = B.composeKleisli(bindIdentity);
+export const composeKleisliFlipped = B.composeKleisliFlipped(bindIdentity);
+export const ifM = B.ifM(bindIdentity);
 
 export const pure: Applicative1<PropIdentity>['pure'] = Identity;
-export const liftA1 = A.liftA1<PropIdentity>({ apply, pure });
-export const when = A.when<PropIdentity>({ pure });
-export const unless = A.unless<PropIdentity>({ pure });
+export const applicativeIdentity: Applicative1<PropIdentity> = { apply, map, pure };
+export const liftA1 = A.liftA1(applicativeIdentity);
+export const when = A.when(applicativeIdentity);
+export const unless = A.unless(applicativeIdentity);
 
-export const liftM1 = M.liftM1<PropIdentity>({ bind, pure });
-export const ap = M.ap<PropIdentity>({ bind, pure });
-export const whenM = M.whenM<PropIdentity>({ bind, pure });
-export const unlessM = M.unlessM<PropIdentity>({ bind, pure });
+export const monadIdentity: Monad1<PropIdentity> = { apply, bind, map, pure };
+export const liftM1 = M.liftM1(monadIdentity);
+export const ap = M.ap(monadIdentity);
+export const whenM = M.whenM(monadIdentity);
+export const unlessM = M.unlessM(monadIdentity);
 
 export const extend: Extend1<PropIdentity>['extend'] = f => x => Identity(f(x));
-export const extendFlipped = Ex.extendFlipped<PropIdentity>({ extend });
-export const composeCoKleisli = Ex.composeCoKleisli<PropIdentity>({ extend });
-export const composeCoKleisliFlipped = Ex.composeCoKleisliFlipped<PropIdentity>({ extend });
-export const duplicate = Ex.duplicate<PropIdentity>({ extend });
+export const extendIdentity: Extend1<PropIdentity> = { extend, map };
+export const extendFlipped = Ex.extendFlipped(extendIdentity);
+export const composeCoKleisli = Ex.composeCoKleisli(extendIdentity);
+export const composeCoKleisliFlipped = Ex.composeCoKleisliFlipped(extendIdentity);
+export const duplicate = Ex.duplicate(extendIdentity);
 
 export const extract: Comonad1<PropIdentity>['extract'] = x => x;
+export const comonadIdentity: Comonad1<PropIdentity> = { extend, extract, map };
