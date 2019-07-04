d00f64bfa0727110852df7ad11e15089ecd8c20d
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-07-04 14:50:21 -0300

    Semigroupoid

---
+++ src/Control/Semigroupoid.ts
@@ -0,0 +1,9 @@
+import { Generic2, Type } from '../Generic';
+
+export interface Semigroupoid<a extends Generic2> {
+	compose: <c, d>(f: Type<a, c, d>) => <b>(g: Type<a, b, c>) => Type<a, b, d>;
+}
+
+export const composeFlipped = <a extends Generic2>(semigroupoid: Semigroupoid<a>) => <b, c>(
+	f: Type<a, b, c>,
+) => <d>(g: Type<a, c, d>): Type<a, b, d> => semigroupoid.compose(g)(f);
