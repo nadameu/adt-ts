cd3e9fe7e5d3b4dda1f555cf7b3e0363f4861b5d
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-07-04 17:23:42 -0300

    Bind2 Helper

--- src/Control/Bind.ts
+++ src/Control/Bind.ts
@@ -10,6 +10,35 @@ export interface Bind2<m extends Generic2> extends Apply2<m> {
 	bind: <a, b>(ma: Type<m, a, b>) => <c>(f: (_: b) => Type<m, a, c>) => Type<m, a, c>;
 }
 
+interface Helpers<m extends Generic1> {
+	bindFlipped: <a, b>(f: (_: a) => Type<m, b>) => (ma: Type<m, a>) => Type<m, b>;
+	join: <a>(mma: Type<m, Type<m, a>>) => Type<m, a>;
+	composeKleisli: <a, b>(
+		f: (_: a) => Type<m, b>,
+	) => <c>(g: (_: b) => Type<m, c>) => (_: a) => Type<m, c>;
+	composeKleisliFlipped: <b, c>(
+		f: (_: b) => Type<m, c>,
+	) => <a>(g: (_: a) => Type<m, b>) => (_: a) => Type<m, c>;
+	ifM: (cond: Type<m, boolean>) => <a>(t: Type<m, a>) => (f: Type<m, a>) => Type<m, a>;
+}
+interface Helpers2<m extends Generic2> {
+	bindFlipped: <a, b, c>(f: (_: b) => Type<m, a, c>) => (ma: Type<m, a, b>) => Type<m, a, c>;
+	join: <a, b>(mma: Type<m, a, Type<m, a, b>>) => Type<m, a, b>;
+	composeKleisli: <a, b, c>(
+		f: (_: b) => Type<m, a, c>,
+	) => <d>(g: (_: c) => Type<m, a, d>) => (_: b) => Type<m, a, d>;
+	composeKleisliFlipped: <a, c, d>(
+		f: (_: c) => Type<m, a, d>,
+	) => <b>(g: (_: b) => Type<m, a, c>) => (_: b) => Type<m, a, d>;
+	ifM: <a>(
+		cond: Type<m, a, boolean>,
+	) => <b>(t: Type<m, a, b>) => (f: Type<m, a, b>) => Type<m, a, b>;
+}
+interface Helper<k extends keyof Helpers<Generic1>> {
+	<m extends Generic1>(bind: Bind<m>): Helpers<m>[k];
+	<m extends Generic2>(bind: Bind2<m>): Helpers2<m>[k];
+}
+
 export const bindFlipped = <m extends Generic1>(
 	bind: Bind<m>,
 ): (<a, b>(f: (_: a) => Type<m, b>) => (ma: Type<m, a>) => Type<m, b>) =>
@@ -21,19 +50,22 @@ export interface Discard<a> {
 	) => (fa: Type<f, a>) => <b>(f: (_: a) => Type<f, b>) => Type<f, b>;
 }
 
-export const join = <m extends Generic1>(
+export const join: Helper<'join'> = <m extends Generic1>(
 	bind: Bind<m>,
 ): (<a>(mma: Type<m, Type<m, a>>) => Type<m, a>) => /*@__PURE__*/ bindFlipped(bind)(identity);
 
-export const composeKleisli = <m extends Generic1>(bind: Bind<m>) => <a, b>(
+export const composeKleisli: Helper<'composeKleisli'> = <m extends Generic1>(bind: Bind<m>) => <
+	a,
+	b
+>(
 	f: (_: a) => Type<m, b>,
 ) => <c>(g: (_: b) => Type<m, c>): ((_: a) => Type<m, c>) => x => bind.bind(f(x))(g);
 
-export const composeKleisliFlipped = <m extends Generic1>(
+export const composeKleisliFlipped: Helper<'composeKleisliFlipped'> = <m extends Generic1>(
 	bind: Bind<m>,
 ): (<b, c>(f: (_: b) => Type<m, c>) => <a>(g: (_: a) => Type<m, b>) => (_: a) => Type<m, c>) =>
 	/*@__PURE__*/ flip<any, any, any>(composeKleisli(bind));
 
-export const ifM = <m extends Generic1>(bind: Bind<m>) => (cond: Type<m, boolean>) => <a>(
-	t: Type<m, a>,
-) => (f: Type<m, a>): Type<m, a> => bind.bind(cond)(c => (c ? t : f));
+export const ifM: Helper<'ifM'> = <m extends Generic1>(bind: Bind<m>) => (
+	cond: Type<m, boolean>,
+) => <a>(t: Type<m, a>) => (f: Type<m, a>): Type<m, a> => bind.bind(cond)(c => (c ? t : f));
