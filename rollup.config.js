8f2952190032dbf52167b10b7697eb3a913c4a2a
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-08-09 17:42:07 -0300

    Project

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,29 @@
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import typescript from "rollup-plugin-typescript";
-import pkg from "./package.json";
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
 
-export default {
-  input: "src/index.ts",
-  plugins: [typescript()],
-  output: [
-    { file: pkg.main, format: "cjs" },
-    { file: pkg.module, format: "es" }
-  ]
-};
+export default [
+  {
+    input: 'src/index.ts',
+    plugins: [
+      typescript({
+        tsconfig: 'src/tsconfig.json',
+        target: 'es5',
+        module: 'es2015',
+        declaration: false
+      })
+    ],
+    output: [{ file: pkg.main, format: 'cjs' }]
+  },
+  {
+    input: 'src/index.ts',
+    plugins: [
+      typescript({
+        tsconfig: 'src/tsconfig.json',
+        target: 'es2018',
+        module: 'es2015',
+        rootDir: './src/'
+      })
+    ],
+    output: [{ file: pkg.module, format: 'es', preferConst: true }]
+  }
+];
