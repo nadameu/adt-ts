dfa9ac41a35e77819400822212748e15fc1fa530
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-08-10 19:00:01 -0300

    Correction

--- src/tree.ts
+++ src/tree.ts
@@ -1,13 +1,6 @@
-import {
-  applicativeMaybe,
-  Just,
-  maybe,
-  Maybe,
-  monadErrorMaybe,
-  Nothing
-} from './Maybe';
+import { applicativeMaybe, Just, maybe, Maybe, monadErrorMaybe, Nothing } from './Maybe';
 
 const x = [Just(42), Nothing as Maybe<number>]
   .map(maybe.traverse(applicativeMaybe)(x => Just(x > 3)))
   .map(monadErrorMaybe.bind(x => x))
-  .reduce((acc, x) => maybe.alt(acc)(x), Nothing);
+  .reduce((acc, x) => maybe.alt(acc, x), Nothing);
