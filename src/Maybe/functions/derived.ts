c4abdbce264e52f4bdd0ab734b1524c559091ae2
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-08-12 19:00:29 -0300

    join

--- src/Maybe/functions/derived.ts
+++ src/Maybe/functions/derived.ts
@@ -1,6 +1,10 @@
-import { fold as foldDefault, foldMapDefaultL } from '../../typeclasses/Foldable';
-import { foldableMaybe, functorMaybe } from '../instances';
+import { lift2 as lift2Default } from '../../typeclasses/Apply';
+import { join as joinDefault } from '../../typeclasses/Bind';
+import { fold as foldDefault } from '../../typeclasses/Foldable';
 import { mapTo as mapToDefault } from '../../typeclasses/Functor';
+import { applyMaybe, bindMaybe, foldableMaybe, functorMaybe } from '../instances';
 
 export const mapTo = mapToDefault(functorMaybe);
 export const fold = foldDefault(foldableMaybe);
+export const lift2 = lift2Default(applyMaybe);
+export const join = joinDefault(bindMaybe);
--- src/typeclasses/Bind.ts
+++ src/typeclasses/Bind.ts
@@ -4,3 +4,7 @@ import { Apply1 } from './Apply';
 export interface Bind1<f extends Generic1> extends Apply1<f> {
   bind: <a, b>(f: (_: a) => Type1<f, b>) => (fa: Type1<f, a>) => Type1<f, b>;
 }
+
+export const join = <f extends Generic1>(
+  bind: Bind1<f>
+): (<a>(ffa: Type1<f, Type1<f, a>>) => Type1<f, a>) => /*#__PURE__*/ bind.bind(x => x);
