b25357b19b394092e439060d2814008377786145
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-08-13 01:10:50 -0300

    Semigroup, Monoid laws

---
+++ test/laws/Monoid.ts
@@ -0,0 +1,17 @@
+import jsc from 'jsverify';
+import { eqNumber } from '../../src';
+import { Generic1, Type1 } from '../../src/Generic';
+import { Eq } from '../../src/typeclasses/Eq';
+import { Monoid, Monoid1 } from '../../src/typeclasses/Monoid';
+
+export const makeMonoidLaws = <a>(monoid: Monoid<a>) => (eq: Eq<a>) => (arb: jsc.Arbitrary<a>) => {
+  return {
+    leftUnit: (): void => jsc.assertForall(arb, x => eq.eq(monoid.append(monoid.mempty(), x), x)),
+    rightUnit: (): void => jsc.assertForall(arb, x => eq.eq(monoid.append(x, monoid.mempty()), x)),
+  };
+};
+
+export const makeMonoid1Laws = <f extends Generic1>(monoid: Monoid1<f>) => (
+  makeEq: <a>(_: Eq<a>) => Eq<Type1<f, a>>
+) => (makeArb: <a>(arb: jsc.Arbitrary<a>) => jsc.Arbitrary<Type1<f, a>>) =>
+  makeMonoidLaws(monoid as any)(makeEq(eqNumber))(makeArb(jsc.number));
---
+++ test/laws/Semigroup.ts
@@ -0,0 +1,24 @@
+import jsc from 'jsverify';
+import { eqNumber } from '../../src';
+import { Generic1, Type1 } from '../../src/Generic';
+import { Eq } from '../../src/typeclasses/Eq';
+import { Semigroup, Semigroup1 } from '../../src/typeclasses/Semigroup';
+
+export const makeSemigroupLaws = <a>(semigroup: Semigroup<a>) => (eq: Eq<a>) => (
+  arb: jsc.Arbitrary<a>
+) => {
+  return {
+    associativity: (): void =>
+      jsc.assertForall(arb, arb, arb, (x, y, z) =>
+        eq.eq(
+          semigroup.append(semigroup.append(x, y), z),
+          semigroup.append(x, semigroup.append(y, z))
+        )
+      ),
+  };
+};
+
+export const makeSemigroup1Laws = <f extends Generic1>(semigroup: Semigroup1<f>) => (
+  makeEq: <a>(_: Eq<a>) => Eq<Type1<f, a>>
+) => (makeArb: <a>(arb: jsc.Arbitrary<a>) => jsc.Arbitrary<Type1<f, a>>) =>
+  makeSemigroupLaws(semigroup as any)(makeEq(eqNumber))(makeArb(jsc.number));
