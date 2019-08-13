c8b747a88dafa045ac6253af7fbb49a021131e25
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-08-13 01:10:35 -0300

    test Number

---
+++ test/Number.test.ts
@@ -0,0 +1,10 @@
+import jsc from 'jsverify';
+import { eqNumber } from '../src/Number';
+import { makeEqLaws } from './laws/Eq';
+
+describe('Eq', () => {
+  const eqLaws = makeEqLaws(eqNumber)(jsc.number);
+  test('Eq - reflexivity', eqLaws.reflexivity);
+  test('Eq - symmetry', eqLaws.symmetry);
+  test('Eq - transitivity', eqLaws.transitivity);
+});
