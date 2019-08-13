2507afb6f3cef50d95f6cf9d7aadadbc730bba12
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-08-13 12:50:14 -0300

    test

--- src/tree.ts
+++ src/tree.ts
@@ -4,3 +4,5 @@ const x = [Just(42), Nothing as Maybe<number>]
   .map(maybe.traverse(applicativeMaybe)(x => Just(x > 3)))
   .map(monadErrorMaybe.bind(x => x))
   .reduce((acc, x) => maybe.alt(acc, x), Nothing);
+
+console.log(x);
