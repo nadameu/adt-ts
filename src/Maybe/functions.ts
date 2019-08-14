3bc80362c0e299963f6132708c46cbb921613104
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-08-14 18:32:40 -0300

    Refactoring

--- src/Maybe/functions.ts
+++ src/Maybe/functions.ts
@@ -1,17 +1,19 @@
 import { Alt1 } from '../typeclasses/Alt';
 import { Applicative1 } from '../typeclasses/Applicative';
-import { Apply1 } from '../typeclasses/Apply';
 import { Bind1 } from '../typeclasses/Bind';
 import { Foldable1, foldlDefault, foldrDefault } from '../typeclasses/Foldable';
-import { Functor1 } from '../typeclasses/Functor';
+import { ap, liftM1, Monad1 } from '../typeclasses/Monad';
 import { MonadError1 } from '../typeclasses/MonadError';
 import { MonadThrow1 } from '../typeclasses/MonadThrow';
 import { Monoid, Monoid1 } from '../typeclasses/Monoid';
 import { Plus1 } from '../typeclasses/Plus';
-import { Traversable1, traverseDefaultFoldablePlus } from '../typeclasses/Traversable';
+import {
+  sequenceDefault,
+  Traversable1,
+  traverseDefaultFoldablePlus,
+} from '../typeclasses/Traversable';
 import { Just, Maybe, Nothing } from './definitions';
 import { TMaybe } from './internal';
-import { liftM1, ap, Monad1 } from '../typeclasses/Monad';
 
 export const maybe = <b>(b: b) => <a>(f: (_: a) => b) => (fa: Maybe<a>): b =>
   fa.isNothing ? b : f(fa.value);
@@ -20,9 +22,9 @@ export const bind: Bind1<TMaybe>['bind'] = /*#__PURE__*/ maybe(Nothing as Maybe<
 
 export const pure: Applicative1<TMaybe>['pure'] = Just;
 
-export const map = liftM1({ bind, pure } as Monad1<TMaybe>);
+export const map = /*#__PURE__*/ liftM1({ bind, pure } as Monad1<TMaybe>);
 
-export const apply = ap({ bind, pure } as Monad1<TMaybe>);
+export const apply = /*#__PURE__*/ ap({ bind, pure } as Monad1<TMaybe>);
 
 export const maybeL = <b>(f: () => b) => <a>(g: (_: a) => b) => (fa: Maybe<a>): b =>
   fa.isNothing ? f() : g(fa.value);
@@ -43,11 +45,15 @@ export const catchError: MonadError1<TMaybe>['catchError'] = f => fa => (fa.isNo
 export const foldMap: Foldable1<TMaybe>['foldMap'] = <m>(monoid: Monoid<m> | Monoid1<any>) =>
   /*#__PURE__*/ maybeL(monoid.mempty);
 
-export const foldl = foldlDefault({ Generic1Type: TMaybe, foldMap });
+export const foldl = /*#__PURE__*/ foldlDefault({ Generic1Type: TMaybe, foldMap });
 
-export const foldr = foldrDefault({ Generic1Type: TMaybe, foldMap });
+export const foldr = /*#__PURE__*/ foldrDefault({ Generic1Type: TMaybe, foldMap });
 
-export const traverse = traverseDefaultFoldablePlus({ Generic1Type: TMaybe, alt, empty, foldMap });
+export const traverse = /*#__PURE__*/ traverseDefaultFoldablePlus({
+  Generic1Type: TMaybe,
+  alt,
+  empty,
+  foldMap,
+});
 
-export const sequence: Traversable1<TMaybe>['sequence'] = applicative =>
-  /*#__PURE__*/ traverse(applicative)(x => x);
+export const sequence = /*#__PURE__*/ sequenceDefault({ traverse } as Traversable1<TMaybe>);
