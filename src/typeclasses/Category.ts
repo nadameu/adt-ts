3c9a246a1252a8d277dc546e901ab5166a4d42bf
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-08-16 18:01:34 -0300

    Correction

--- src/typeclasses/Category.ts
+++ src/typeclasses/Category.ts
@@ -2,7 +2,7 @@ import { GenericFn, TypeFn } from '../Generic';
 import { Semigroupoid2 } from './Semigroupoid';
 
 export interface Category2<c extends GenericFn> extends Semigroupoid2<c> {
-  id: <a>(
+  identity: <a>(
     _: TypeFn<c, a, a> extends (_: infer a) => any ? a : never
   ) => TypeFn<c, a, a> extends (_: any) => infer a ? a : never;
 }
