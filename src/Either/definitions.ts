3325e72bc1d7993374476042f2331fcb2f2f4a30
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-08-20 13:27:35 -0300

    Refactoring

--- src/Either/definitions.ts
+++ src/Either/definitions.ts
@@ -6,32 +6,11 @@ export interface Left<a> {
   isRight: false;
   leftValue: a;
 }
+export const Left = <a>(leftValue: a): Left<a> => ({ isLeft: true, isRight: false, leftValue });
 
 export interface Right<b> {
   isLeft: false;
   isRight: true;
   rightValue: b;
 }
-
-const make: {
-  (isLeft: true): <a>(leftValue: a) => Left<a>;
-  (isLeft: false): <b>(rightValue: b) => Right<b>;
-} = (isLeft: boolean) => {
-  const proto = Object.create(Either.prototype);
-  proto.isLeft = isLeft;
-  proto.isRight = !isLeft;
-  if (isLeft)
-    return function Left<a>(leftValue: a) {
-      const left = Object.create(proto);
-      left.leftValue = leftValue;
-      return left;
-    };
-  return function Right<b>(rightValue: b) {
-    const right = Object.create(proto);
-    right.rightValue = rightValue;
-    return right;
-  };
-};
-
-export const Left = /*#__PURE__*/ make(true);
-export const Right = /*#__PURE__*/ make(false);
+export const Right = <b>(rightValue: b): Right<b> => ({ isLeft: false, isRight: true, rightValue });
