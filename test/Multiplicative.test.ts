573c3645a65169f399b6dab74beb987b2712c6ba
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-08-20 17:48:49 -0300

    Formatting

--- test/Multiplicative.test.ts
+++ test/Multiplicative.test.ts
@@ -1,8 +1,8 @@
 import * as jsc from 'jsverify';
 import { monoidMultiplicative, semigroupMultiplicative } from '../src/Multiplicative';
-import { makeSemigroupLaws } from './laws/Semigroup';
 import { eqNumber } from '../src/Number';
 import { makeMonoidLaws } from './laws/Monoid';
+import { makeSemigroupLaws } from './laws/Semigroup';
 
 describe('Semigroup', () => {
   const semigroupLaws = makeSemigroupLaws(semigroupMultiplicative)(eqNumber)(jsc.int16);
