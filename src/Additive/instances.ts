5f0c7e1fcd750972b1ec4d21233fc3862bb8ccb4
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-08-20 12:55:38 -0300

    Refactoring

--- src/Additive/instances.ts
+++ src/Additive/instances.ts
@@ -1,3 +1,8 @@
-import * as monoidAdditive from './instances/monoid';
-import * as semigroupAdditive from './instances/semigroup';
-export { monoidAdditive, semigroupAdditive };
+import { Monoid } from '../typeclasses/Monoid';
+import { Semigroup } from '../typeclasses/Semigroup';
+
+const append = (x: number, y: number) => x + y;
+
+export const semigroupAdditive = { append } as Semigroup<number>;
+
+export const monoidAdditive = { append, mempty: () => 0 } as Monoid<number>;
--- src/Additive/instances/semigroup.ts
+++
@@ -1,5 +0,0 @@
-import { Semigroup } from '../../typeclasses/Semigroup';
-
-export let NotGenericType: number;
-
-export const append: Semigroup<number>['append'] = (x, y) => x + y;
