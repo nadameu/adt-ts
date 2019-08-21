a97be50c9346adcfa5e80c32d2dd4148aa40bde4
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-08-21 12:39:31 -0300

    monoidString

--- src/String/functions.ts
+++ src/String/functions.ts
@@ -1,2 +1,10 @@
 import { Eq } from '../typeclasses/Eq';
+import { Semigroup } from '../typeclasses/Semigroup';
+import { curry2 } from '../curry';
+import { Monoid } from '../typeclasses/Monoid';
+
 export const eq: Eq<string>['eq'] = (a, b) => a === b;
+
+export const append: Semigroup<string>['append'] = curry2((x, y) => `${x}${y}`);
+
+export const mempty: Monoid<string>['mempty'] = () => '';
--- src/String/instances.ts
+++ src/String/instances.ts
@@ -1,4 +1,7 @@
 import { Eq } from '../typeclasses/Eq';
-import { eq } from './functions';
+import { Monoid } from '../typeclasses/Monoid';
+import { append, eq, mempty } from './functions';
 
 export const eqString = { eq } as Eq<string>;
+
+export const monoidString = { append, mempty } as Monoid<string>;
