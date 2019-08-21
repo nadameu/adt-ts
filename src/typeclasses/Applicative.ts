10b0bf69c1a67ca1f3fa952dc9923a627100602a
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-08-21 12:54:26 -0300

    Refactoring

--- src/typeclasses/Applicative.ts
+++ src/typeclasses/Applicative.ts
@@ -1,13 +1,13 @@
+import { curry3 } from '../curry';
 import { Generic1, Generic2, Type1, Type2 } from '../Generic';
 import { Apply1, Apply2 } from './Apply';
-import { curry2 } from '../curry';
 
 export interface Applicative1<f extends Generic1> extends Apply1<f> {
-  pure: Helpers1<f>['pure'];
+  pure: <a>(a: a) => Type1<f, a>;
 }
 
 export interface Applicative2<f extends Generic2> extends Apply2<f> {
-  pure: Helpers2<f>['pure'];
+  pure: <a, b>(b: b) => Type2<f, a, b>;
 }
 
 export type AnyApplicative = Pick<
@@ -15,28 +15,43 @@ export type AnyApplicative = Pick<
   keyof Applicative1<Generic1> & keyof Applicative2<Generic2>
 >;
 
-interface Helpers1<f extends Generic1> {
-  pure: <a>(a: a) => Type1<f, a>;
-  liftA1: Applicative1<f>['map'];
-}
-interface Helpers2<f extends Generic2> {
-  pure: <a, b>(b: b) => Type2<f, a, b>;
-  liftA1: Applicative2<f>['map'];
-}
-type Helper = {
-  [k in keyof Helpers1<never>]: {
-    <f extends Generic1>(applicative: Applicative1<f>): Helpers1<f>[k];
-    <f extends Generic2>(applicative: Applicative2<f>): Helpers2<f>[k];
-  };
+type ApplyPure1<f extends Generic1> = {
+  [k in 'Generic1Type' | 'apply' | 'pure']: Applicative1<f>[k];
 };
-type PartialHelper<keys extends keyof Applicative1<never> & keyof Applicative2<never>> = {
-  [k in keyof Helpers1<never>]: {
-    <f extends Generic1>(_: Pick<Applicative1<f>, 'Generic1Type' | keys>): Helpers1<f>[k];
-    <f extends Generic2>(_: Pick<Applicative2<f>, 'Generic2Type' | keys>): Helpers2<f>[k];
-  };
+type ApplyPure2<f extends Generic2> = {
+  [k in 'Generic2Type' | 'apply' | 'pure']: Applicative2<f>[k];
 };
 
-export const liftA1: PartialHelper<'apply' | 'pure'>['liftA1'] = ({
-  apply,
-  pure,
-}: Pick<AnyApplicative, 'apply' | 'pure'>) => curry2((f, fa) => apply(pure(f))(fa));
+export const liftA1: {
+  <f extends Generic1, a, b>(
+    { apply, pure }: ApplyPure1<f>,
+    f: (_: a) => b,
+    fa: Type1<f, a>
+  ): Type1<f, b>;
+  <f extends Generic1, a, b>({ apply, pure }: ApplyPure1<f>, f: (_: a) => b): (
+    fa: Type1<f, a>
+  ) => Type1<f, b>;
+  <f extends Generic1>({ apply, pure }: ApplyPure1<f>): {
+    <a, b>(f: (_: a) => b, fa: Type1<f, a>): Type1<f, b>;
+    <a, b>(f: (_: a) => b): (fa: Type1<f, a>) => Type1<f, b>;
+  };
+
+  <f extends Generic2, a, b, c>(
+    { apply, pure }: ApplyPure2<f>,
+    f: (_: b) => c,
+    fa: Type2<f, a, b>
+  ): Type2<f, a, c>;
+  <f extends Generic2, a, b, c>({ apply, pure }: ApplyPure2<f>, f: (_: b) => c): (
+    fa: Type2<f, a, b>
+  ) => Type2<f, a, c>;
+  <f extends Generic2>({ apply, pure }: ApplyPure2<f>): {
+    <a, b, c>(f: (_: b) => c, fa: Type2<f, a, b>): Type2<f, a, c>;
+    <a, b, c>(f: (_: b) => c): (fa: Type2<f, a, b>) => Type2<f, a, c>;
+  };
+} = curry3(
+  (
+    { apply, pure }: Pick<Applicative1<Generic1> & Applicative2<Generic2>, 'apply' | 'pure'>,
+    f,
+    fa
+  ) => apply(pure(f))(fa)
+);
