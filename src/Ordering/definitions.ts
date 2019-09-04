303338cdc04fad63675602ef0640a6ab11125d12
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-09-04 16:59:03 -0300

    Ordering exports

--- src/Ordering/definitions.ts
+++ src/Ordering/definitions.ts
@@ -1,5 +1,2 @@
-export const enum Ordering {
-  LT = -1,
-  EQ = 0,
-  GT = +1,
-}
+export const Ordering = { LT: -1, EQ: 0, GT: +1 } as const;
+export type Ordering = typeof Ordering[keyof typeof Ordering];
