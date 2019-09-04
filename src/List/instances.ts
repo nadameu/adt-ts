7d7a965afd789f39ad5570cbe6ceced49bc47a64
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-09-04 16:57:33 -0300

    Non-empty list instances

--- src/List/instances.ts
+++ src/List/instances.ts
@@ -2,33 +2,36 @@ import { Alt1 } from '../typeclasses/Alt';
 import { Alternative1 } from '../typeclasses/Alternative';
 import { Applicative1 } from '../typeclasses/Applicative';
 import { Apply1 } from '../typeclasses/Apply';
+import { Bind1 } from '../typeclasses/Bind';
+import { Eq } from '../typeclasses/Eq';
+import { Foldable1 } from '../typeclasses/Foldable';
+import { Foldable1_1 } from '../typeclasses/Foldable1';
 import { Functor1 } from '../typeclasses/Functor';
+import { Monad1 } from '../typeclasses/Monad';
 import { Monoid1 } from '../typeclasses/Monoid';
 import { Plus1 } from '../typeclasses/Plus';
 import { Semigroup1 } from '../typeclasses/Semigroup';
+import { Traversable1 } from '../typeclasses/Traversable';
+import { List, ListTag, NEList } from './definitions';
 import {
   alt,
   append,
   apply,
+  bind,
   empty,
+  fold1,
+  foldl,
+  foldMap,
+  foldMap1,
+  foldr,
   map,
   mempty,
   pure,
-  bind,
-  foldl,
-  foldr,
-  foldMap,
-  traverse,
   sequence,
+  traverse,
   uncons,
 } from './functions/original';
 import { TList, TNEList } from './internal';
-import { Bind1 } from '../typeclasses/Bind';
-import { Foldable1 } from '../typeclasses/Foldable';
-import { Monad1 } from '../typeclasses/Monad';
-import { Traversable1 } from '../typeclasses/Traversable';
-import { Eq } from '../typeclasses/Eq';
-import { List, ListTag } from './definitions';
 
 /* LIST */
 export const makeEqList = <a>(eq: Eq<a>) =>
@@ -64,6 +67,8 @@ export const traversableList = { foldMap, foldl, foldr, map, sequence, traverse 
 >;
 
 /* NELIST */
+export const makeEqNEList = makeEqList as <a>(eq: Eq<a>) => Eq<NEList<a>>;
+
 export const functorNEList = functorList as Functor1<TNEList>;
 export const applyNEList = applyList as Apply1<TNEList>;
 export const applicativeNEList = applicativeList as Applicative1<TNEList>;
@@ -72,5 +77,9 @@ export const monadNEList = monadList as Monad1<TNEList>;
 
 export const semigroupNEList = { append } as Semigroup1<TNEList>;
 
+export const altNEList = altList as Alt1<TNEList>;
+
 export const foldableNEList = foldableList as Foldable1<TNEList>;
 export const traversableNEList = traversableList as Traversable1<TNEList>;
+
+export const foldable1NEList = { fold1, foldMap, foldMap1, foldl, foldr } as Foldable1_1<TNEList>;
