2ae8969d49010413191b23cee6fe864c4fc559fe
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-09-16 16:44:58 -0300

    Explicit dependencies

--- src/Alternate/instances.ts
+++ src/Alternate/instances.ts
@@ -1,27 +1,29 @@
 import { Anon, Generic1, Generic2 } from '../Generic';
 import {
+  AltOnly_1,
+  AltOnly_2,
   Alt_1,
-  Alt_2,
+  EmptyOnly_1,
   Monoid_1,
   Monoid_2,
   Plus_1,
-  Plus_2,
   Semigroup_1,
   Semigroup_2,
 } from '../typeclasses';
+import { EmptyOnly_2 } from '../typeclasses/Plus';
 
 export const makeSemigroupAlternate: {
-  <f extends Generic1>(alt: Alt_1<f>): Semigroup_1<f>;
-  <f extends Generic2>(alt: Alt_2<f>): Semigroup_2<f>;
+  <f extends Generic1>({ alt }: AltOnly_1<f>): Semigroup_1<f>;
+  <f extends Generic2>({ alt }: AltOnly_2<f>): Semigroup_2<f>;
 } = <f extends Generic1>({ alt }: Anon<Alt_1<f>, 'alt'>) =>
   ({
     append: alt,
   } as Semigroup_1<Generic1> & Semigroup_2<Generic2>);
 
 export const makeMonoidAlternate: {
-  <f extends Generic1>(plus: Plus_1<f>): Monoid_1<f>;
-  <f extends Generic2>(plus: Plus_2<f>): Monoid_2<f>;
-} = <f extends Generic1>({ alt, empty }: Anon<Plus_1<f>>) =>
+  <f extends Generic1>({ alt, empty }: AltOnly_1<f> & EmptyOnly_1<f>): Monoid_1<f>;
+  <f extends Generic2>({ alt, empty }: AltOnly_2<f> & EmptyOnly_2<f>): Monoid_2<f>;
+} = <f extends Generic1>({ alt, empty }: Anon<Plus_1<f>, 'alt' | 'empty'>) =>
   ({
     append: alt,
     mempty: empty,
