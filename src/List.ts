d1ad2263e75a9250e9462338e214b7fe12bd0754
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-09-18 19:13:20 -0300

    Correction

--- src/List.ts
+++ src/List.ts
@@ -1,4 +1,4 @@
-export { ConsList, List, NEList, SnocList } from './List/definitions';
+export { List, Cons, Nil } from './List/definitions';
 export * from './List/instances';
 import * as list from './List/functions';
 export { list };
