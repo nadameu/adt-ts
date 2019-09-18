2884fbbac9ecf35a0c50ea3ef6eb8601975a9f6d
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-09-18 02:54:31 -0300

    Refactoring

--- src/Array/functions/original.ts
+++ src/Array/functions/original.ts
@@ -9,158 +9,95 @@ import {
   Bind_1,
   compactByFilterMap,
   Filterable_1,
-  filterDefault,
-  FilterMapOnly_1,
   Foldable_1,
+  FoldLOnly_1,
   foldMapDefaultR,
   Functor_1,
+  GenericSnoc_1,
   Monoid_1,
   partitionDefault,
   Semigroup_1,
   separateByPartitionMap,
   sequenceDefault,
   Traversable_1,
+  traverseDefaultSnoc,
   Witherable_1,
 } from '../../typeclasses';
 import { TArray } from '../internal';
 
-export const forEach = <a>(f: (_: a) => void) => (xs: ArrayLike<a>) => {
-  const len = xs.length;
-  for (let i = 0; i < len; i++) {
-    f(xs[i]);
-  }
-};
+const borrow = <
+  key extends keyof Array<any>,
+  args extends unknown[] = Array<any>[key] extends (...args: infer args) => unknown
+    ? args
+    : unknown[],
+  b = Array<any>[key] extends (_: any) => infer b ? b : unknown
+>(
+  key: key,
+  ...args: args
+) => (xs: ArrayLike<unknown>): b => (Array.prototype[key] as any).apply(xs, args);
+
+const snocArray = <a>(xs: a[]) => (x: a) => (xs.push(x), xs);
+
+export const forEach = <a>(f: (_: a) => void): ((xs: ArrayLike<a>) => void) =>
+  borrow('forEach', x => f(x));
 
 export const forEachRight = <a>(f: (_: a) => void) => (xs: ArrayLike<a>) => {
-  for (let i = xs.length - 1; i >= 0; i--) {
-    f(xs[i]);
-  }
+  for (let i = xs.length - 1; i > -1; i--) f(xs[i]);
 };
 
-export const forEachWithIndex = <a>(f: (_: number) => (_: a) => void) => (xs: ArrayLike<a>) => {
-  const len = xs.length;
-  for (let i = 0; i < len; i++) {
-    f(i)(xs[i]);
-  }
-};
+export const forEachWithIndex = <a>(
+  f: (_: number) => (_: a) => void
+): ((xs: ArrayLike<a>) => void) => borrow('forEach', (x, i) => f(i)(x));
 
-export const bind: Bind_1<TArray>['bind'] = <a, b>(f: (_: a) => ArrayLike<b>) => (
-  xs: ArrayLike<a>
-): ArrayLike<b> => {
-  const ys: b[] = [];
-  forEach<a>(x => {
-    forEach<b>(y => {
-      ys.push(y);
-    })(f(x));
-  })(xs);
-  return ys;
-};
+export const bind: Bind_1<TArray>['bind'] = <a, b>(
+  f: (_: a) => ArrayLike<b>
+): ((xs: ArrayLike<a>) => ArrayLike<b>) =>
+  foldl<a, b[]>(ys => x => foldl<b, b[]>(snocArray)(ys)(f(x)))([]);
 
-export const map: Functor_1<TArray>['map'] = <a, b>(f: (_: a) => b) => (
-  xs: ArrayLike<a>
-): ArrayLike<b> => {
-  const ys: b[] = new Array(xs.length);
-  let i = 0;
-  forEach<a>(x => {
-    ys[i++] = f(x);
-  })(xs);
-  return ys;
-};
+export const map: Functor_1<TArray>['map'] = <a, b>(
+  f: (_: a) => b
+): ((xs: ArrayLike<a>) => ArrayLike<b>) => borrow('map', x => f(x));
 
-export const mapWithIndex = <a, b>(f: (_: number) => (_: a) => b) => (
-  xs: ArrayLike<a>
-): ArrayLike<b> => {
-  const ys: b[] = new Array(xs.length);
-  forEachWithIndex<a>(i => x => {
-    ys[i] = f(i)(x);
-  })(xs);
-  return ys;
-};
+export const mapWithIndex = <a, b>(
+  f: (_: number) => (_: a) => b
+): ((xs: ArrayLike<a>) => ArrayLike<b>) => borrow('map', (x, i) => f(i)(x));
 
 export const apply: Apply_1<TArray>['apply'] = <a, b>(fs: ArrayLike<(_: a) => b>) => (
   xs: ArrayLike<a>
-): ArrayLike<b> => {
-  const ys: b[] = new Array(fs.length * xs.length);
-  let i = 0;
-  forEach<(_: a) => b>(f => {
-    forEach<a>(x => {
-      ys[i++] = f(x);
-    })(xs);
-  })(fs);
-  return ys;
-};
+): ArrayLike<b> =>
+  foldl<(_: a) => b, b[]>(ys => f => foldl<a, b[]>(ys => x => snocArray(ys)(f(x)))(ys)(xs))([])(fs);
 
 export const pure: Applicative_1<TArray>['pure'] = x => [x];
 
-export const foldl: Foldable_1<TArray>['foldl'] = <a, b>(f: (_: b) => (_: a) => b) => (b: b) => (
-  xs: ArrayLike<a>
-): b => {
-  let acc = b;
-  forEach<a>(x => {
-    acc = f(acc)(x);
-  })(xs);
-  return acc;
-};
+export const foldl: Foldable_1<TArray>['foldl'] = <a, b>(f: (_: b) => (_: a) => b) => (
+  b: b
+): ((xs: ArrayLike<a>) => b) => borrow('reduce', (acc: b, x: a) => f(acc)(x), b);
 
-export const foldlWithIndex = <a, b>(f: (_: b) => (_: number) => (_: a) => b) => (b: b) => (
-  xs: ArrayLike<a>
-): b => {
-  let acc = b;
-  forEachWithIndex<a>(i => x => {
-    acc = f(acc)(i)(x);
-  })(xs);
-  return acc;
-};
+export const foldlWithIndex = <a, b>(f: (_: b) => (_: number) => (_: a) => b) => (
+  b: b
+): ((xs: ArrayLike<a>) => b) => borrow('reduce', (acc: b, x: a, i) => f(acc)(i)(x), b);
 
-export const foldr: Foldable_1<TArray>['foldr'] = <a, b>(f: (_: a) => (_: b) => b) => (b: b) => (
-  xs: ArrayLike<a>
-): b => {
-  let acc = b;
-  forEachRight<a>(x => {
-    acc = f(x)(acc);
-  })(xs);
-  return acc;
-};
+export const foldr: Foldable_1<TArray>['foldr'] = <a, b>(f: (_: a) => (_: b) => b) => (
+  b: b
+): ((xs: ArrayLike<a>) => b) => borrow('reduceRight', (acc: b, x: a) => f(x)(acc), b);
 
-export const foldrWithIndex = <a, b>(f: (_: number) => (_: a) => (_: b) => b) => (b: b) => (
-  xs: ArrayLike<a>
-): b => {
-  let acc = b;
-  let i = xs.length;
-  forEachRight<a>(x => {
-    acc = f(--i)(x)(acc);
-  })(xs);
-  return acc;
-};
+export const foldrWithIndex = <a, b>(f: (_: number) => (_: a) => (_: b) => b) => (
+  b: b
+): ((xs: ArrayLike<a>) => b) => borrow('reduceRight', (acc: b, x: a, i) => f(i)(x)(acc), b);
 
 export const foldMap = foldMapDefaultR({ foldr } as Foldable_1<TArray>);
 
-export const append: Semigroup_1<TArray>['append'] = <a>(xs: ArrayLike<a>) => (
-  ys: ArrayLike<a>
-) => {
-  const result: a[] = new Array(xs.length + ys.length);
-  let i = 0;
-  forEach<a>(x => {
-    result[i++] = x;
-  })(xs);
-  forEach<a>(y => {
-    result[i++] = y;
-  })(ys);
-  return result;
-};
+export const append: Semigroup_1<TArray>['append'] = <a>(
+  xs: ArrayLike<a>
+): ((ys: ArrayLike<a>) => ArrayLike<a>) => foldl<a, a[]>(snocArray)(Array.from(xs));
 
 export const mempty: Monoid_1<TArray>['mempty'] = () => [];
 
-export const traverse: Traversable_1<TArray>['traverse'] = <f extends Generic1>({
-  apply,
-  map,
-  pure,
-}: Anon<Applicative_1<f>>) => <a, b>(f: (_: a) => Type1<f, b>) => (
-  as: ArrayLike<a>
-): Type1<f, ArrayLike<b>> =>
-  foldrWithIndex<a, Type1<f, b[]>>(i => x =>
-    lift2({ apply, map } as Apply_1<f>)((b: b) => (bs: b[]) => ((bs[i] = b), bs))(f(x))
-  )(pure(new Array(as.length)))(as);
+export const traverse = traverseDefaultSnoc(({
+  foldl,
+  nil: mempty,
+  snoc: <a>(xs: a[]) => (x: a) => (xs.push(x), xs),
+} as unknown) as FoldLOnly_1<TArray> & GenericSnoc_1<TArray>);
 
 export const sequence = sequenceDefault({ traverse } as Traversable_1<TArray>);
 
@@ -168,18 +105,11 @@ export const alt = append;
 
 export const empty = mempty;
 
-export const filterMap: Filterable_1<TArray>['filterMap'] = <a, b>(f: (_: a) => Maybe<b>) => (
-  as: ArrayLike<a>
-): ArrayLike<b> => {
-  const bs: b[] = [];
-  forEach<a>(a => {
-    const mb = f(a);
-    if (mb.isJust) bs.push(mb.value);
-  })(as);
-  return bs;
-};
+export const filter = <a>(p: (_: a) => boolean): ((fa: ArrayLike<a>) => ArrayLike<a>) =>
+  borrow('filter', x => p(x));
 
-export const filter = filterDefault({ filterMap } as FilterMapOnly_1<TArray>);
+export const filterMap = <a, b>(p: (_: a) => Maybe<b>): ((fa: ArrayLike<a>) => ArrayLike<b>) =>
+  foldl<a, b[]>(ys => x => (y => (y.isJust && ys.push(y.value), ys))(p(x)))([]);
 
 export const compact = compactByFilterMap({ filterMap } as Filterable_1<TArray>);
 
@@ -201,9 +131,9 @@ export const separate = separateByPartitionMap({ partitionMap } as Filterable_1<
 export const wither: Witherable_1<TArray>['wither'] = <m extends Generic1>(
   applicative: Anon<Applicative_1<m>>
 ) => <a, b>(f: (_: a) => Type1<m, Maybe<b>>): ((as: ArrayLike<a>) => Type1<m, ArrayLike<b>>) => {
-  const lifted = lift2(applicative as Applicative_1<m>)<Maybe<b>, b[], b[]>(mb => bs =>
-    mb.isJust ? (bs.push(mb.value), bs) : bs
-  );
+  const lifted = lift2(applicative as Applicative_1<m>)<Maybe<b>, b[], b[]>(mb => bs => (
+    mb.isJust && bs.push(mb.value), bs
+  ));
   return foldl(flip((a: a) => lifted(f(a))))(applicative.pure([]));
 };
 
@@ -226,15 +156,9 @@ export const wilt: Witherable_1<TArray>['wilt'] = <m extends Generic1>(
 export const range = (start: number) => (end: number): number[] => {
   if (!Number.isInteger(start) || !Number.isInteger(end))
     throw new TypeError('Start and end must be integers.');
+  let x = start;
   const diff = end - start;
-  if (diff < 0) {
-    const xs = new Array(1 - diff);
-    const past = end - 1;
-    for (let i = 0, x = start; x > past; i++, x--) xs[i] = x;
-    return xs;
-  }
-  const xs = new Array(diff + 1);
-  const past = end + 1;
-  for (let i = 0, x = start; x < past; i++, x++) xs[i] = x;
-  return xs;
+  return diff < 0
+    ? Array.from({ length: 1 - diff }, _ => x--)
+    : Array.from({ length: diff + 1 }, _ => x++);
 };
