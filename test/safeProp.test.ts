bc1a2f0884ddc9b0d80130478c99a20ed620c015
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-09-20 01:06:49 -0300

    Refactoring

--- test/safeProp.test.ts
+++ test/safeProp.test.ts
@@ -13,7 +13,33 @@ interface MightHaveC {
   z: number;
 }
 
-test('With pipeK', () => {
+const runTests = (
+  a: (_: MightHaveA) => Maybe<MightHaveB>,
+  b: (_: MightHaveA) => Maybe<MightHaveC>,
+  c: (_: MightHaveA) => Maybe<string>
+) => {
+  const obj1: MightHaveA = { x: 42 };
+  expect(a(obj1)).toEqual(Nothing);
+  expect(b(obj1)).toEqual(Nothing);
+  expect(c(obj1)).toEqual(Nothing);
+
+  const obj2: MightHaveA = { a: { y: 43 }, x: 42 };
+  expect(a(obj2)).toEqual(Just(obj2.a));
+  expect(b(obj2)).toEqual(Nothing);
+  expect(c(obj2)).toEqual(Nothing);
+
+  const obj3: MightHaveA = { a: { b: { z: 44 }, y: 43 }, x: 42 };
+  expect(a(obj3)).toEqual(Just(obj3.a));
+  expect(b(obj3)).toEqual(Just(obj3.a!.b));
+  expect(c(obj3)).toEqual(Nothing);
+
+  const obj4: MightHaveA = { a: { b: { c: 'Hello!', z: 44 }, y: 43 }, x: 42 };
+  expect(a(obj4)).toEqual(Just(obj4.a));
+  expect(b(obj4)).toEqual(Just(obj4.a!.b));
+  expect(c(obj4)).toEqual(Just(obj4.a!.b!.c));
+};
+
+test('With pipeK and safeProp', () => {
   const a: (_: MightHaveA) => Maybe<MightHaveB> = maybe.pipeK(maybe.safeProp('a'));
   const b: (_: MightHaveA) => Maybe<MightHaveC> = maybe.pipeK(
     maybe.safeProp('a'),
@@ -24,33 +50,10 @@ test('With pipeK', () => {
     maybe.safeProp('b'),
     maybe.safeProp('c')
   );
-
-  const obj1: MightHaveA = { x: 42 };
-
-  expect(a(obj1)).toEqual(Nothing);
-  expect(b(obj1)).toEqual(Nothing);
-  expect(c(obj1)).toEqual(Nothing);
-
-  const obj2: MightHaveA = { a: { y: 43 }, x: 42 };
-
-  expect(a(obj2)).toEqual(Just(obj2.a));
-  expect(b(obj2)).toEqual(Nothing);
-  expect(c(obj2)).toEqual(Nothing);
-
-  const obj3: MightHaveA = { a: { b: { z: 44 }, y: 43 }, x: 42 };
-
-  expect(a(obj3)).toEqual(Just(obj3.a));
-  expect(b(obj3)).toEqual(Just(obj3.a!.b));
-  expect(c(obj3)).toEqual(Nothing);
-
-  const obj4: MightHaveA = { a: { b: { c: 'Hello!', z: 44 }, y: 43 }, x: 42 };
-
-  expect(a(obj4)).toEqual(Just(obj4.a));
-  expect(b(obj4)).toEqual(Just(obj4.a!.b));
-  expect(c(obj4)).toEqual(Just(obj4.a!.b!.c));
+  runTests(a, b, c);
 });
 
-test('With pipe', () => {
+test('With pipe and bindProp', () => {
   const a: (_: MightHaveA) => Maybe<MightHaveB> = pipe(maybe.safeProp('a'));
   const b: (_: MightHaveA) => Maybe<MightHaveC> = pipe(
     maybe.safeProp('a'),
@@ -62,27 +65,5 @@ test('With pipe', () => {
     maybe.bindProp('c')
   );
 
-  const obj1: MightHaveA = { x: 42 };
-
-  expect(a(obj1)).toEqual(Nothing);
-  expect(b(obj1)).toEqual(Nothing);
-  expect(c(obj1)).toEqual(Nothing);
-
-  const obj2: MightHaveA = { a: { y: 43 }, x: 42 };
-
-  expect(a(obj2)).toEqual(Just(obj2.a));
-  expect(b(obj2)).toEqual(Nothing);
-  expect(c(obj2)).toEqual(Nothing);
-
-  const obj3: MightHaveA = { a: { b: { z: 44 }, y: 43 }, x: 42 };
-
-  expect(a(obj3)).toEqual(Just(obj3.a));
-  expect(b(obj3)).toEqual(Just(obj3.a!.b));
-  expect(c(obj3)).toEqual(Nothing);
-
-  const obj4: MightHaveA = { a: { b: { c: 'Hello!', z: 44 }, y: 43 }, x: 42 };
-
-  expect(a(obj4)).toEqual(Just(obj4.a));
-  expect(b(obj4)).toEqual(Just(obj4.a!.b));
-  expect(c(obj4)).toEqual(Just(obj4.a!.b!.c));
+  runTests(a, b, c);
 });
