a018f8ce2fdef91771b2623d7aae6a3435f45139
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2020-11-03 18:45:38 -0300

    String semigroup

--- src/String/instances.ts
+++ src/String/instances.ts
@@ -1,6 +1,7 @@
-import { Eq, Monoid_0 } from '../typeclasses';
+import { Eq, Monoid_0, Semigroup_0 } from '../typeclasses';
 import { append, eq, mempty } from './functions';
 
 export const eqString = { eq } as Eq<string>;
 
+export const semigroupString = { append } as Semigroup_0<string>;
 export const monoidString = { append, mempty } as Monoid_0<string>;
