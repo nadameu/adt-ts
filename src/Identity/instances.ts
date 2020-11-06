7297ba5aed3039db35beed502144598360e8a624
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2020-11-06 01:38:48 -0300

    Refactoring

--- src/Identity/instances.ts
+++ src/Identity/instances.ts
@@ -1,11 +1,18 @@
+import { identity } from '../helpers/identity';
 import { Applicative_1, Apply_1, Bind_1, Functor_1, Monad_1 } from '../typeclasses';
 import { TIdentity } from './internal';
 
-const map = <a, b>(f: (_: a) => b) => (a: a) => f(a);
-const pure = <a>(a: a) => a;
-
-export const functorIdentity = { map } as Functor_1<TIdentity>;
-export const applyIdentity = { apply: map, map } as Apply_1<TIdentity>;
-export const applicativeIdentity = { apply: map, map, pure } as Applicative_1<TIdentity>;
-export const bindIdentity = { apply: map, bind: map, map } as Bind_1<TIdentity>;
-export const monadIdentity = { apply: map, bind: map, map, pure } as Monad_1<TIdentity>;
+export const functorIdentity = { map: identity } as Functor_1<TIdentity>;
+export const applyIdentity = { apply: identity, map: identity } as Apply_1<TIdentity>;
+export const applicativeIdentity = {
+  apply: identity,
+  map: identity,
+  pure: identity,
+} as Applicative_1<TIdentity>;
+export const bindIdentity = { apply: identity, bind: identity, map: identity } as Bind_1<TIdentity>;
+export const monadIdentity = {
+  apply: identity,
+  bind: identity,
+  map: identity,
+  pure: identity,
+} as Monad_1<TIdentity>;
