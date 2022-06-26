3f575b4ef2fac7fadd406f92ecca393e61440991
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2022-06-26 01:16:48 -0300

    Custom functor for Free tests

--- test/Free.test.ts
+++ test/Free.test.ts
@@ -1,60 +1,84 @@
 import * as fc from 'fast-check';
 import { describe, test } from 'vitest';
-import {
-  Free,
-  functorIdentity,
-  identity,
-  Join,
-  makeEqFree,
-  makeFunctorFree,
-  makeMonadFree,
-  Pure,
-} from '../src';
+import { Free, Join, makeEqFree, makeFunctorFree, makeMonadFree, Pure } from '../src';
 import { TFree } from '../src/Free/internal';
-import { TIdentity } from '../src/Identity/internal';
+import { Generic1 } from '../src/Generic';
 import { Eq } from '../src/typeclasses';
+import { makeEq } from '../src/typeclasses/Eq';
+import { makeFunctor } from '../src/typeclasses/Functor';
 import { makeApplicative1Laws } from './laws/Applicative';
 import { makeApply1Laws } from './laws/Apply';
 import { makeEq1Laws } from './laws/Eq';
 import { makeFunctor1Laws } from './laws/Functor';
 import { makeMonad1Laws } from './laws/Monad';
 
-const functorFreeIdentity = makeFunctorFree(functorIdentity);
-const monadFreeIdentity = makeMonadFree(functorIdentity);
+interface MightFail<i, a> {
+  initialValue: i | null;
+  mapping: (_: i) => a;
+}
+function MightFail<i, a>(initialValue: i | null, mapping: (_: i) => a): MightFail<i, a> {
+  return { initialValue, mapping };
+}
+interface TMF extends Generic1 {
+  type: MightFail<any, this['a']>;
+}
+const functorMightFail = makeFunctor<TMF>({
+  map: f => fx => MightFail(fx.initialValue, i => f(fx.mapping(i))),
+});
 
-const makeEqFreeIdentity = <a>(eqA: Eq<a>) => makeEqFree(eqA, identity as any);
+const functorFreeMightFail = makeFunctorFree(functorMightFail);
+const monadFreeMightFail = makeMonadFree(functorMightFail);
+const makeEqMightFail = <a>(eqA: Eq<a>): Eq<MightFail<any, a>> =>
+  makeEq({
+    eq: x => y => {
+      if (x.initialValue === null)
+        if (y.initialValue === null) return true;
+        else return false;
+      if (y.initialValue === null) return false;
+      else return eqA.eq(x.mapping(x.initialValue))(y.mapping(y.initialValue));
+    },
+  });
 
-const makeArb = <a>(arb: fc.Arbitrary<a>) =>
-  fc
-    .array(fc.constant(Join), { minLength: 0, maxLength: 3 })
-    .chain(levels => levels.reduce((x, f) => x.map(f), arb.map<Free<TIdentity, a>>(Pure)));
+const makeEqFreeMightFail = <a>(eqA: Eq<a>) => makeEqFree<TMF, a>(eqA, makeEqMightFail);
+
+const makeArb = <a>(arb: fc.Arbitrary<a>) => {
+  const pure = arb.map<Free<TMF, a>>(Pure);
+  const tr: (_: fc.Arbitrary<Free<TMF, a>>) => fc.Arbitrary<Free<TMF, a>> = x =>
+    fc.oneof(fc.constant(null), x).map(x => Join(MightFail(x, i => i)));
+
+  const f = fc
+    .array(fc.constant(tr), { minLength: 0, maxLength: 0 })
+    .chain(xs => xs.reduce((x, f) => f(x), pure));
+
+  return f;
+};
 
 describe('Functor', () => {
-  const functorLaws = makeFunctor1Laws(functorFreeIdentity)(makeEqFreeIdentity)(makeArb);
+  const functorLaws = makeFunctor1Laws(functorFreeMightFail)(makeEqFreeMightFail)(makeArb);
   test('Functor - identity', functorLaws.identity);
   test('Functor - composition', functorLaws.composition);
 });
 
 describe('Apply', () => {
-  const applyLaws = makeApply1Laws(monadFreeIdentity)(makeEqFreeIdentity)(makeArb);
+  const applyLaws = makeApply1Laws(monadFreeMightFail)(makeEqFreeMightFail)(makeArb);
   test('Apply - composition', applyLaws.composition);
 });
 
 describe('Applicative', () => {
-  const applicativeLaws = makeApplicative1Laws(monadFreeIdentity)(makeEqFreeIdentity)(makeArb);
+  const applicativeLaws = makeApplicative1Laws(monadFreeMightFail)(makeEqFreeMightFail)(makeArb);
   test('Applicative - identity', applicativeLaws.identity);
   test('Applicative - homomorphism', applicativeLaws.homomorphism);
   test('Applicative - interchange', applicativeLaws.interchange);
 });
 
 describe('Monad', () => {
-  const monadLaws = makeMonad1Laws(monadFreeIdentity)(makeEqFreeIdentity)(makeArb);
+  const monadLaws = makeMonad1Laws(monadFreeMightFail)(makeEqFreeMightFail)(makeArb);
   test('Monad - left identity', monadLaws.leftIdentity);
   test('Monad - right identity', monadLaws.rightIdentity);
 });
 
 describe('Eq', () => {
-  const eqLaws = makeEq1Laws<TFree<TIdentity>>(x => makeEqFree(x, identity as any))(makeArb);
+  const eqLaws = makeEq1Laws<TFree<TMF>>(makeEqFreeMightFail)(makeArb);
   test('Eq - reflexivity', eqLaws.reflexivity);
   test('Eq - symmetry', eqLaws.symmetry);
   test('Eq - transitivity', eqLaws.transitivity);
